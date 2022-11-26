411b9009053daebbab63f64ab4db4445585afe63
mdisoft126 <mdisoft126@gmail.com>
2022-11-26 23:18:46 +0100

    chapter 1 introduction

---
+++ test.js
@@ -0,0 +1,18 @@
+//// shortcuts:
+// ctr + F10 --> run
+// clg --> console.log
+// shift + alt + down arrow --> duplicate line
+// shift + ctrl + k --> remove line
+
+// var a = "said \n\"hello\"!";
+// console.log(typeof a);
+// console.log(a);
+
+var a = '1\r2';
+var b = '1\n\r2';
+var c = '1\r\n2';
+console.log(a);
+console.log(b);
+console.log(c);
+
+//// page 48 Type Boolean
