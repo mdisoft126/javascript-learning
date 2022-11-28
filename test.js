dfd421436fabc053fb88bb06a5fe02529fd1db0b
Marcin Dolecki <marcin.dolecki@kramphub.com>
2022-11-28 23:54:22 +0100

    logical operators

--- test.js
+++ test.js
@@ -4,15 +4,7 @@
 // shift + alt + down arrow --> duplicate line
 // shift + ctrl + k --> remove line
 
-// var a = "said \n\"hello\"!";
-// console.log(typeof a);
-// console.log(a);
+var a = "";
+console.log(!!a);
 
-var a = '1\r2';
-var b = '1\n\r2';
-var c = '1\r\n2';
-console.log(a);
-console.log(b);
-console.log(c);
-
-//// page 48 Type Boolean
+// Page 50
