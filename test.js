c3411c8ced9b4374d525bb26c3c39be14c610793
mdisoft126 <mdisoft126@gmail.com>
2022-12-06 00:09:01 +0100

    Page 80 Decode Encode URIs

--- test.js
+++ test.js
@@ -4,28 +4,7 @@
 // shift + alt + down arrow --> duplicate line
 // shift + ctrl + k --> remove line
 
-////// multiplication table
-//// header
-var res = '';
-for (i = 0; i <= 10; i++) {
-    if (i == 0) {
-        res += i;
-    } else {
-        res += '    ';
-        res += i;
-    }
-}
-console.log(res + '\n');
-//// main
-var res = '';
-for (i = 1; i <= 10; i++) {
-    res += i;
-    for (j = 1; j <= 10; j++) {
-        res += '    ';
-        res += (i * j);
-    }
-    res += '\n' + '\n';
-}
-console.log(res);
-////
-// Page 72 --> do the exercise 3!
+console.log(isNaN(parseInt('a123')));
+
+
+// Page 80 --> Encode Decode URIs
