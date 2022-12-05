71c16d3f58c36e72de86a6f85c251b86529fc130
mdisoft126 <mdisoft126@gmail.com>
2022-12-05 23:05:58 +0100

    Multiplication table

--- test.js
+++ test.js
@@ -4,10 +4,28 @@
 // shift + alt + down arrow --> duplicate line
 // shift + ctrl + k --> remove line
 
-var a = 1;
-a = delete a;
-console.log(typeof a);
-var a = a || 10;
-
-console.log(a);
+////// multiplication table
+//// header
+var res = '';
+for (i = 0; i <= 10; i++) {
+    if (i == 0) {
+        res += i;
+    } else {
+        res += '    ';
+        res += i;
+    }
+}
+console.log(res + '\n');
+//// main
+var res = '';
+for (i = 1; i <= 10; i++) {
+    res += i;
+    for (j = 1; j <= 10; j++) {
+        res += '    ';
+        res += (i * j);
+    }
+    res += '\n' + '\n';
+}
+console.log(res);
+////
 // Page 72 --> do the exercise 3!
