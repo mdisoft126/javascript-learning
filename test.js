81c3e757120bba46a1e41a358b9b11f8b5f2e723
mdisoft126 <mdisoft126@gmail.com>
2022-12-07 00:02:28 +0100

    functions

--- test.js
+++ test.js
@@ -4,7 +4,20 @@
 // shift + alt + down arrow --> duplicate line
 // shift + ctrl + k --> remove line
 
-console.log(isNaN(parseInt('a123')));
+function addOne (a) {
+    return a + 1;
+}
+
+function multiplyByTwo (a, b, c, f) {
+    var i, res = [];
+    for (i = 0; i < 3; i++) {
+        res[i] = f(arguments[i] * 2);
+    }
+    return console.log(res);
+}
+
+multiplyByTwo(1,2,3, addOne);
+multiplyByTwo(1,2,3, function(a){return a + 2});
 
 
-// Page 80 --> Encode Decode URIs
+// Page 87 --> Functions
