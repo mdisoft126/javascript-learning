f44bb9eddee658c5b703b7808ef28b69257262bc
mdisoft126 <mdisoft126@gmail.com>
2022-12-07 23:47:17 +0100

    Functions - range

--- test.js
+++ test.js
@@ -4,20 +4,17 @@
 // shift + alt + down arrow --> duplicate line
 // shift + ctrl + k --> remove line
 
-function addOne (a) {
-    return a + 1;
+function f1() {
+    var a = 1;
+    console.log(a);
+    f2();
 }
 
-function multiplyByTwo (a, b, c, f) {
-    var i, res = [];
-    for (i = 0; i < 3; i++) {
-        res[i] = f(arguments[i] * 2);
-    }
-    return console.log(res);
+function f2() {
+    return console.log(a);
 }
 
-multiplyByTwo(1,2,3, addOne);
-multiplyByTwo(1,2,3, function(a){return a + 2});
+var a = 5;
+f1();
 
-
-// Page 87 --> Functions
+// Page 93 --> Functions range
