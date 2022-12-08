818e29d802f31d497eb4ef4941ab5fc6a2ce6a42
mdisoft126 <mdisoft126@gmail.com>
2022-12-08 23:36:23 +0100

    functions - clousures

--- test.js
+++ test.js
@@ -4,17 +4,23 @@
 // shift + alt + down arrow --> duplicate line
 // shift + ctrl + k --> remove line
 
-function f1() {
-    var a = 1;
-    console.log(a);
-    f2();
+function f() {
+    function c(x) {
+        return function() {
+            return x;
+        }
+    }
+    var a = [];
+    var i;
+    for(i = 0; i < 3; i++) {
+        a[i] = c(i);
+    }
+    return a;
 }
 
-function f2() {
-    return console.log(a);
-}
-
-var a = 5;
-f1();
+var n = f();
+console.log(n[0]());
+console.log(n[1]());
+console.log(n[2]());
 
 // Page 93 --> Functions range
