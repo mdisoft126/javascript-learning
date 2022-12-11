4b4f659bba2d41559c79696e5fe939900736d753
mdisoft126 <mdisoft126@gmail.com>
2022-12-11 20:58:14 +0100

    regexp - modifiers and groups

--- regexp.js
+++ regexp.js
@@ -3,13 +3,12 @@
 // https://www.w3schools.com/jsref/jsref_obj_regexp.asp
 
 var a = "E6t7665D, BluEw, rfed,1 REd, orrrrange, he9j, BLUe, blue,";
-var p = /e/i;
-var s = p.exec(a);
+var p = /[^a-z]/g;
+var s = a.match(p);
 
-// console.log(p.global);
+console.log(s);
 
-console.log("Found letter: " + s[0] + " in position: " + s.index + " from input: " + s.input);
 
 // Status:
 // next this one:
-// https://www.w3schools.com/jsref/jsref_regexp_m.asp
+// https://www.w3schools.com/jsref/jsref_regexp_dot.asp
