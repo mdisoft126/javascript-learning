c12d3673e3fc2fa15148d1eed143c4debcf33617
mdisoft126 <mdisoft126@gmail.com>
2022-12-11 23:09:33 +0100

    regexp - quantifiers finished

--- regexp.js
+++ regexp.js
@@ -2,11 +2,11 @@
 // https://www.w3schools.com/js/js_regexp.asp
 // https://www.w3schools.com/jsref/jsref_obj_regexp.asp
 
-var a = "E6t7665D, Blu\n\vEw, rfed,1 REd, orr\n\vrrange, he9j, BLUe, blue,";
-var p = /[^a-z]/g;
+var a = "E6t7665D, Blu\n\vEwE, rfed,1 RE987d, orr\n\vr88range, he9j, BLUe, blue,";
+var p = /e(?!, )/ig;
 var s = a.match(p);
 
-console.log(a);
+console.log(s);
 
 
 // Status:
