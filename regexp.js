ebb02fa3e1174d9b5c5907fcc6810c66d1e7a160
mdisoft126 <mdisoft126@gmail.com>
2022-12-11 22:46:43 +0100

    regexp - metacharacters finished

--- regexp.js
+++ regexp.js
@@ -2,13 +2,13 @@
 // https://www.w3schools.com/js/js_regexp.asp
 // https://www.w3schools.com/jsref/jsref_obj_regexp.asp
 
-var a = "E6t7665D, BluEw, rfed,1 REd, orrrrange, he9j, BLUe, blue,";
+var a = "E6t7665D, Blu\n\vEw, rfed,1 REd, orr\n\vrrange, he9j, BLUe, blue,";
 var p = /[^a-z]/g;
 var s = a.match(p);
 
-console.log(s);
+console.log(a);
 
 
 // Status:
 // next this one:
-// https://www.w3schools.com/jsref/jsref_regexp_dot.asp
+// https://www.w3schools.com/jsref/jsref_regexp_onemore.asp
