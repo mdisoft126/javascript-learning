a18468dc2c778931f3a84adc8b7084319c6c835f
mdisoft126 <mdisoft126@gmail.com>
2022-12-11 23:29:23 +0100

    regexp - properties finished

--- regexp.js
+++ regexp.js
@@ -2,9 +2,9 @@
 // https://www.w3schools.com/js/js_regexp.asp
 // https://www.w3schools.com/jsref/jsref_obj_regexp.asp
 
-var a = "E6t7665D, Blu\n\vEwE, rfed,1 RE987d, orr\n\vr88range, he9j, BLUe, blue,";
-var p = /e(?!, )/ig;
-var s = a.match(p);
+var a = "hello blue Hello RED";
+var p = /hello/gi;
+var s = p.source;
 
 console.log(s);
 
