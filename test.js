fc96a9d2593d93216a21e8d942e4c0c5c1a42319
mdisoft126 <mdisoft126@gmail.com>
2022-12-13 23:43:41 +0100

    exercise4

--- test.js
+++ test.js
@@ -5,17 +5,22 @@
 // shift + ctrl + k --> remove line
 
 ////
-
-var a = 1;
-function f() {
-  var a = 2;
-  function n() {
-    console.log(a);
+var f = console.log
+f("hello");
+////
+// 4.1
+var f = console.log
+eval('f("hello2")');
+// 4.2
+var e;
+var f = console.log;
+eval('e=f')("hello3");
+// 4.3
+(
+  function() {
+    return console.log;
   }
-  n();
-}
-
-f();
+)()("hello4");
 
 
 // Page 101 --> Exercises nr 2
