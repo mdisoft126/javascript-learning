6f3e95c1c56e79d29b920d204d88e627a8903772
Marcin Dolecki <marcin.dolecki@kramphub.com>
2022-12-15 16:42:13 +0100

    clg when when selected

--- test.js
+++ test.js
@@ -1,8 +1,9 @@
 //// shortcuts:
 // ctr + F10 --> run
 // clg --> console.log
-// shift + alt + down arrow --> duplicate line
+// shift + alt + down arrow --> duplicate line / ctrl + d --> duplicate line
 // shift + ctrl + k --> remove line
+// {selected code} + ctrl + shift + l --> insert console log
 
 ////
 
