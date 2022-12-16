8a63cf0bbc153d486872673fca587ea881e75753
Marcin Dolecki <marcin.dolecki@kramphub.com>
2022-12-16 22:09:04 +0100

    field contructor

--- test.js
+++ test.js
@@ -7,39 +7,14 @@
 
 ////
 
-// function Work() {
-//   this.specialization = 'Analyst';
-// }
+function Animal(species, name) {this.species = species; this.name = name}
+var a = new Animal("dog", "Rex");
+var b = new Animal("cat", "Kitty");
+var c = new b.constructor("hamster", "Puszka");
+console.log(c);
 
-// var work = new Work();
-// console.log(work.specialization);
-
-// function Animal(species, name) {
-//   this.species = species;
-//   this.name = name;
-//   this.whatAnimal = function() {
-//     return "My species is " + species + " and my name is " + name;
-//   }
-// }
-
-// var dog = new Animal("dog", "max");
-// var cat = new Animal("cat", "kitty");
-
-// console.log(dog.whatAnimal());
-// console.log(cat.whatAnimal());
-
-function Work(name, position) {
-  this.name = name;
-  this.position = position;
-  this.whatNameAndPosition = function() {
-    return "My name is " + name + " and my position is " + position;
-  }
-}
-
-var marcin = new Work("Marcin", "analyst");
-var justyn = new Work("Justyn", "journalist");
-
-console.log(marcin.whatNameAndPosition());
-console.log(justyn.whatNameAndPosition());
+var o = {};
+console.log(o.constructor);
+console.log(typeof(o.constructor));
 
 // Page 110
