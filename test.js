2c9ddba5bd40a2825f40ca3a10a721b23f98027a
Marcin Dolecki <marcin.dolecki@kramphub.com>
2022-12-18 14:00:45 +0100

    Built-in objects - array

--- test.js
+++ test.js
@@ -6,50 +6,27 @@
 // {selected code} + ctrl + shift + l --> insert console log
 
 ////
-var a = [];
-a[0] = 55;
-a[1] = 66;
-console.log(a[0]);
-
-var b = [5, 6, 1, 'abc'];
+var a = [3,6,3,8,'test'];
+console.log(a.push(8));
+console.log(a);
+console.log(a.pop());
+console.log(a);
+a[a.length] = 8;
+console.log(a);
+a.length--;
+console.log(a);
+var b = a.sort();
+console.log(a);
 console.log(b);
-
-var c = new Array(7, 2, 'ggg');
-console.log(c);
-
-var d = [5];
-console.log(d);
-
-var e = new Array(5);
-console.log(e);
-
-console.log(typeof a);
-
-console.log(b.toString());
-
-console.log(c.valueOf());
-
-console.log(a.constructor);
-
-var o = {name: "max", species: "dog"};
-console.log(o.length);
-console.log(c.length);
-
-var f = [];
-var p = {};
-console.log(f[0] = 1);
-console.log(p[0] = 1);
-console.log(f.prop = 2);
-console.log(p.prop = 2);
-console.log(f);
-console.log(p);
-console.log(f.length);
-f[2] = 'a';
-f[3] = 'b';
-console.log(f);
-console.log(f.length);
-f.length = 7;
-console.log(f);
+console.log(a.join(' and '));
+console.log(a);
+b = a.slice(1,4);
+console.log(a);
+console.log(b);
+a.splice(1,3, 'jey', 102, 'hoho');
+console.log(a);
+a.splice(1,1);
+console.log(a);
 
 
-// Page 117 Built-in objects
+// Page 122 Built-in objects - Function
