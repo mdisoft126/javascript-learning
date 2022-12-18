94cbd4947f159931bde6edd2ac97df3473ebab3f
Marcin Dolecki <marcin.dolecki@kramphub.com>
2022-12-18 12:28:54 +0100

    built-in objects - object

--- test.js
+++ test.js
@@ -6,7 +6,11 @@
 // {selected code} + ctrl + shift + l --> insert console log
 
 ////
-
-
+var a = {};
+var b = new Object();
+var c = {};
+console.log(b.toString());
+console.log(typeof("An object " + a));
+console.log(c.valueOf() === c);
 
 // Page 117 Built-in objects
