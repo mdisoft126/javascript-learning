3e947c9ea5dd3cb6a479290bbe13dd842233ce3c
Marcin Dolecki <marcin.dolecki@kramphub.com>
2022-12-18 23:04:02 +0100

    a new look at the arguments object

--- test.js
+++ test.js
@@ -6,24 +6,26 @@
 // {selected code} + ctrl + shift + l --> insert console log
 
 ////
-function sum(a, b, c) {return a + b + c};
-console.log(sum.toString());
+function f(a, b, c) {return arguments};
+console.log(f(4,2,8));
+console.log(f.length);
 
-console.log(eval.toString());
+function f2(a, b) {return arguments.callee};
+console.log(f(3,1));
 
-var obj = {
-    name: 'max',
-    say: function(who) {return "Hello " + who + " I am " + this.name}
-}
-console.log(obj.say("Martin"));
+function f3() {return arguments};
+console.log(f3());
 
-var my_obj = {
-    name: 'Leonidas papanidas'
-}
+function f4() {return arguments.callee};
+console.log(f4());
 
-console.log(obj.say.call(my_obj, 'Michael'));
-console.log(obj.say.call('Michael'));
+(
+    function(count) {
+        if (count < 5) {
+            console.log("hello " + count);
+            arguments.callee(++count);
+        }
+    }
+)(1)
 
-console.log(obj.say.apply(my_obj, ['Miguel']));
-
-// Page 122 Built-in objects - Function
+// Page 127 Boolean
