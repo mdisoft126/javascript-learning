e6462027df39f7c0fc612bd1ead441200ee9e048
Marcin Dolecki <marcin.dolecki@kramphub.com>
2022-12-19 23:20:17 +0100

    number

--- test.js
+++ test.js
@@ -6,24 +6,38 @@
 // {selected code} + ctrl + shift + l --> insert console log
 
 ////
-var b = new Boolean();
+var a = Number('12.12');
+console.log(a);
+console.log(typeof a);
+
+var b = new Number('12.12');
 console.log(b);
 console.log(typeof b);
-console.log(typeof b.valueOf());
-console.log(b.valueOf());
 
-console.log(Boolean("test"));
-console.log(Boolean(""));
-console.log(Boolean({}));
+console.log(Number.MAX_VALUE);
+console.log(Number.MIN_VALUE);
+console.log(Number.POSITIVE_INFINITY);
+console.log(Number.NEGATIVE_INFINITY);
+console.log(Number.NaN);
+console.log(Number.MAX_SAFE_INTEGER);
+console.log(Number.MIN_SAFE_INTEGER);
 
-var a = new Boolean(true);
-console.log(a.valueOf());
-var b = new Boolean(false);
-console.log(b.valueOf());
+var a = new Number(123.736);
+console.log(a.toFixed(2));
 
-console.log(a);
+console.log((34.653).toFixed(1));
 
-console.log(Boolean(a));
-console.log(Boolean(b));
+console.log((1736.768).toExponential());
+
+console.log((753.246).toPrecision(2));
+
+var n = new Number(255);
+console.log(n.toString());
+console.log(n.toString(10));
+console.log(n.toString(16));
+console.log(n.toString(2));
+console.log(n.toString(8));
+
+console.log((6).toString(2));
 
 // Page 127 Boolean
