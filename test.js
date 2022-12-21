0d8bc57e97e5f98da5ed4f705f78076e849c531b
Marcin Dolecki <marcin.dolecki@kramphub.com>
2022-12-21 23:19:15 +0100

    math

--- test.js
+++ test.js
@@ -6,21 +6,42 @@
 // {selected code} + ctrl + shift + l --> insert console log
 
 ////
-var a = new String('master of disaster');
-console.log(a.valueOf());
-console.log(typeof a.split(' '));
-var b = "master of disaster";
-console.log(b);
-console.log(typeof b);
+console.log(Math.PI);
+console.log(Math.SQRT2);
+console.log(Math.E);
+console.log(Math.LN2);
+console.log(Math.LN10);
 
-var a1 = a.split(" ");
-console.log(a1);
-var b1 = b.split(" ");
-console.log(b1);
-console.log(typeof a1.join(" "));
-console.log(typeof b1.join(" "));
+console.log('');
 
-console.log(a.concat(" it is me!"));
+console.log(Math.random());
+console.log("between 0 and 100: " + (100 * Math.random()));
+console.log("between 50 and 100: " + (50 * Math.random() + 50));
+console.log(1 * Math.random() + 5);
+
+console.log('');
+
+var a = 100 * Math.random();
 console.log(a);
+console.log(Math.round(a));
+console.log(Math.floor(a));
+console.log(Math.ceil(a));
 
-// Page 133 (end of the page) Methods slice and substring
+console.log('');
+
+console.log(Math.min(8, 16));
+console.log(Math.max(8, 16));
+console.log(Math.min());
+console.log(Math.max());
+var a = -5;
+console.log(Math.min(Math.max(1, a), 12));
+
+console.log('');
+
+console.log(Math.pow(2, 3));
+console.log(Math.sqrt(9));
+console.log(Math.sin(0.524));
+console.log(Math.sin(Math.PI/6));
+
+
+// Page 136 Date
