f2b54a1bd380df9c838dac107395cd507bb5931e
Marcin Dolecki <marcin.dolecki@kramphub.com>
2022-12-21 23:26:59 +0100

    date

--- test.js
+++ test.js
@@ -6,42 +6,10 @@
 // {selected code} + ctrl + shift + l --> insert console log
 
 ////
-console.log(Math.PI);
-console.log(Math.SQRT2);
-console.log(Math.E);
-console.log(Math.LN2);
-console.log(Math.LN10);
-
-console.log('');
-
-console.log(Math.random());
-console.log("between 0 and 100: " + (100 * Math.random()));
-console.log("between 50 and 100: " + (50 * Math.random() + 50));
-console.log(1 * Math.random() + 5);
-
-console.log('');
-
-var a = 100 * Math.random();
+var a = new Date();
 console.log(a);
-console.log(Math.round(a));
-console.log(Math.floor(a));
-console.log(Math.ceil(a));
 
-console.log('');
-
-console.log(Math.min(8, 16));
-console.log(Math.max(8, 16));
-console.log(Math.min());
-console.log(Math.max());
-var a = -5;
-console.log(Math.min(Math.max(1, a), 12));
-
-console.log('');
-
-console.log(Math.pow(2, 3));
-console.log(Math.sqrt(9));
-console.log(Math.sin(0.524));
-console.log(Math.sin(Math.PI/6));
+console.log(new Date('1 3 2022 1:00'));
 
 
 // Page 136 Date
