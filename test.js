0df43a4825e912e85b2d4e10b82837263917f53e
Marcin Dolecki <marcin.dolecki@kramphub.com>
2022-12-26 23:41:06 +0100

    built-in object - regexp - replace

--- test.js
+++ test.js
@@ -7,10 +7,20 @@
 
 ////
 var a = new String("HelloJavaScriptWorld");
-console.log(a.match(/a/g));
-console.log(a.match(/a/g)['index']);
-console.log(a.match(/j.*s/i));
-console.log(a.search(/j.*s/i));
+console.log(a);
+var r = /[A-Z]/g;
+console.log(a.replace(r, ''));
+console.log(a.replace(r, '_$&'));
+console.log(a.replace(r, '$&_'));
+var r2 = /([A-Z])(a)/g;
+console.log(a.replace(r2, '_$1-$2'));
+var r3 = /([A-Z])([a-z])/g;
+console.log(a.replace(r3, '_$1-$2'));
+
+var email = "mdi126@wp.pl";
+console.log(email);
+var user = email.replace(/(.*)@.*/, '$1');
+console.log(user);
 
 
 // Page 143 finish .exec()
