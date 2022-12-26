d34daf0519506d3f2045a364928b9023ed8ad4af
Marcin Dolecki <marcin.dolecki@kramphub.com>
2022-12-26 22:10:03 +0100

    built-in object -regexp exec

--- test.js
+++ test.js
@@ -6,10 +6,23 @@
 // {selected code} + ctrl + shift + l --> insert console log
 
 ////
-var a = /h.*r/ig;
+var a = /h/ig;
+console.log(typeof a);
+// console.log(a.test("Hello world"));
+console.log(a.exec("hello"));
+console.log(typeof a);
 console.log(a.test("Hello world"));
-console.log(a.exec("hello")[0]);
+console.log(a.exec("hello"));
+console.log(typeof a);
 
+// var b = /h/ig.exec("hello")[0];
+// console.log(b);
+
+// var a = "E6t7665D, BluEw, rfed,1 REd, orrrrange, he9j, BLUe, blue,";
+// var p = /e/i;
+// var s = p.exec(a);
+
+// console.log(p.exec("E6t7665D, BluEw, rfed,1 REd, orrrrange, he9j, BLUe, blue,"));
 
 
 // Page 143 finish .exec()
