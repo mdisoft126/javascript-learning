f61bc1bb189b3b2ea98cf12bf81fd827414d92a6
Marcin Dolecki <marcin.dolecki@kramphub.com>
2022-12-28 00:00:32 +0100

    built-in object  - error handling

--- test.js
+++ test.js
@@ -6,10 +6,71 @@
 // {selected code} + ctrl + shift + l --> insert console log
 
 ////
-var a = 'test';
-console.log(a.replace('e', 'o'));
-console.log(a);
-console.log(a.replace(new RegExp('e'), 'o'));
+try {
+    hello();
+} catch (e) {
+    console.log(e.name + ": " + e.message);
+} finally {
+    console.log("The end!");
+}
 
+function f() {
+    return 0;
+}
 
-// Page 145 replace callback
+try {
+    var total = f();
+    if(total === 0) {
+        throw new Error('Dividing by O!');
+    } else {
+        console.log(50/total);
+    }
+} catch (e) {
+    console.log(e.name + ": " + e.message);
+} finally {
+    console.log('The end!');
+}
+
+try {
+    var total = f();
+    if(total === 0) {
+        throw new RangeError('Dividing by O!');
+    } else {
+        console.log(50/total);
+    }
+} catch (e) {
+    console.log(e.name + ": " + e.message);
+} finally {
+    console.log('The end!');
+}
+
+try {
+    var total = f();
+    if(total === 0) {
+        throw {
+            name: "My error",
+            message: "Something bad happened"
+        };
+    } else {
+        console.log(50/total);
+    }
+} catch (e) {
+    console.log(e.name + ": " + e.message);
+} finally {
+    console.log('The end!');
+}
+
+try {
+    var total = f();
+    if(total === 0) {
+        throw new Error('Dividing by O!');
+    } else {
+        console.log(50/total);
+    }
+} catch (e) {
+    console.log(e);
+} finally {
+    console.log('The end!');
+}
+
+// Page 151 exercises
