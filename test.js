6b21e30b7a8704d42b260ab31655a4d35c63f27d
Marcin Dolecki <marcin.dolecki@kramphub.com>
2022-12-27 23:19:46 +0100

    built-in object  - regexp - replace callback

--- test.js
+++ test.js
@@ -12,5 +12,14 @@ function replaceCallback(match) {
 }
 console.log(a.replace(/[A-Z]/g, replaceCallback));
 
-
+var glob;
+var r = /(.*)@(.*)\.(.*)/;
+var callback = function() {
+    glob = arguments;
+    return arguments[1] + ' on the server ' + arguments[2] + ' dot ' + arguments[3];
+}
+var eMail = 'mdisoft126@wp.pl';
+console.log(eMail.replace(r, callback));
+console.log(glob);
+console.log(typeof callback);
 // Page 145 replace callback
