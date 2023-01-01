d481fae364be62dc75616de43456396af3f879bc
Marcin Dolecki <marcin.dolecki@kramphub.com>
2023-01-01 23:47:50 +0100

    Object - exercise 4

--- test.js
+++ test.js
@@ -9,81 +9,105 @@
 
 //Exercise 4
 
-// function MyString(input) {
+function MyString(input) {
 
-//     // conversion to string
-//     var a = '';
-//     var str = a + input;
+    // conversion to string
+    var a = '';
+    var str = a + input;
 
-//     // insert each character to table to be able to looking for specyfic characters
-//     var i;
-//     var r = [];
-//     for (i = 0; i < str.length; i++) {
-//         r[i] = str[i];
-//     }
+    // insert each character to table to be able to looking for specyfic characters
+    var i;
+    var r = [];
+    for (i = 0; i < str.length; i++) {
+        r[i] = str[i];
+    }
 
-//     // length
-//     r.lengthMy = str.length;
+    // length
+    r.lengthMy = str.length;
 
-//     // toString
-//     r.toStringMy = function() {
-//         return str;
-//     }
+    // toString
+    r.toStringMy = function() {
+        return str;
+    }
 
-//     // valueOf
-//     r.valueOfMy = function() {
-//         return input;
-//     }
+    // valueOf
+    r.valueOfMy = function() {
+        return input;
+    }
 
-//     // chatAt
-//     r.charAtMy = function(char) {
-//         var a = 0;
-//         if(!isNaN(a + char)) {
-//             return r[char];
-//         } else {
-//             return r[0];
-//         }
-//     }
+    // chatAt
+    r.charAtMy = function(char) {
+        var a = 0;
+        if(!isNaN(a + char)) {
+            return r[char];
+        } else {
+            return r[0];
+        }
+    }
 
-//     // concat
-//     r.concatMy = function(con) {
-//         return input += con;
-//     }
+    // concat
+    r.concatMy = function(con) {
+        return input += con;
+    }
 
-//     // slice
-//     r.sliceMy = function(s, e) {
-//         // var s = s1;
-//         // var e = e1;
-//         if (s < 0) {
-//             s = r.length + s;
-//         }
-//         if (e < 0) {
-//             e = r.length + e;
-//         }
-//         var str = '';
-//         for (i = s; i < e; i++) {
-//             str += r[i];
-//         }
-//         return str;
-//     }
+    // slice
+    r.sliceMy = function(s, e) {
+        // var s = s1;
+        // var e = e1;
+        if (s < 0) {
+            s = r.length + s;
+        }
+        if (e < 0) {
+            e = r.length + e;
+        }
+        var str = '';
+        for (i = s; i < e; i++) {
+            str += r[i];
+        }
+        return str;
+    }
 
-//     // return
-//     return r
-// };
+    // split
+    r.splitMy = function(d) {
 
-// var s = new MyString('hello');
-// console.log(s);
-// console.log(s.lengthMy);
-// console.log(s[0]);
-// console.log(s.toStringMy());
-// console.log(s.valueOfMy());
-// console.log(s.charAtMy(1));
-// console.log(s.charAtMy('2'));
-// console.log(s.charAtMy('e'));
-// console.log(s.concatMy(' world!'));
-// console.log(s.sliceMy(1,3));
-// console.log(s.sliceMy(0,-1));
-// console.log(s.sliceMy(-3,-1));
+        var i;
+        var j = 0;
+        var str = '';
+        var res = [];
+        for (i = 0; i < r.length; i++) {
+            if (r[i] !== d && i === r.length -1) {
+                str += r[i];
+                res[j] = str;
+            } else if (r[i] === d) {
+                res[j] = str;
+                str = '';
+                j++;
+            } else if (r[i] !== d) {
+                str += r[i];
+            }
+        }
+        return res;
+    }
+
+    // return
+    return r
+};
+
+var s = new MyString('hello');
+console.log(s);
+console.log(s.lengthMy);
+console.log(s[0]);
+console.log(s.toStringMy());
+console.log(s.valueOfMy());
+console.log(s.charAtMy(1));
+console.log(s.charAtMy('2'));
+console.log(s.charAtMy('e'));
+console.log(s.concatMy(' world!'));
+console.log(s.sliceMy(1,3));
+console.log(s.sliceMy(0,-1));
+console.log(s.sliceMy(-3,-1));
+console.log(s.splitMy('e'));
+console.log(s.splitMy('l'));
 
 
 
@@ -174,20 +198,27 @@
 //     }
 // }
 
-var a = 'helloello';
-var d = 'l';
-var i;
-var str = '';
-var res = [];
-for (i = 0; i < a.length; i++) {
-    if (a[i] !== d) {
-        str += a[i];
-    } else if  (a[i] === d && i === 0) {
-        res[i] = '';
-    } else {
-        res[]
-    }
-}
+///////// splitMy
+// var a = 'hello';
+// var d = 'l';
+// var i;
+// var j = 0;
+// var str = '';
+// var res = [];
+// for (i = 0; i < a.length; i++) {
+//     if (a[i] !== d && i === a.length -1) {
+//         str += a[i];
+//         res[j] = str;
+//         console.log('hey');
+//     } else if (a[i] === d) {
+//         res[j] = str;
+//         str = '';
+//         j++;
+//     } else if (a[i] !== d) {
+//         str += a[i];
+//     }
+// }
+// console.log(res);
 
 //// Answer:
 
