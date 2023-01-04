c424cb5b8822b7845f6e1ba8763f27be16bf9df9
Marcin Dolecki <marcin.dolecki@kramphub.com>
2023-01-04 01:33:25 +0100

    Object - exercise 6 in progress

--- test.js
+++ test.js
@@ -14,28 +14,38 @@ function MyArray(...args) {
     console.log(arguments);
     var arg = arguments;
 
+    var b;
+
     // toString
-    this.toStringMy = function() {
-        var str = "";
-        for (var a in arg) {
-            if (a < arg.length -1) {
-                str += arg[a] + ',';
-            } else {
-                str += arg[a];
+    arg.toStringMy = function() {
+        function f() {
+            var str = "";
+            for (var a in arg) {
+                if (a < arg.length -1) {
+                    str += arg[a] + ',';
+                } else {
+                    str += arg[a];
+                }
             }
+            return arg.str;
         }
-        return str;
+        f();
     }
 
-    // length
-    this.lengthMy = arg.length;
+    // // length
+    // this.lengthMy = arg.length;
 
-    
 
+
+    // return
+    return arg;
 }
 
 var a = new MyArray(1,2,3,"test");
 console.log(a.toStringMy());
-console.log(a.lengthMy);
+// console.log(a.lengthMy);
+// console.log(a);
+console.log(a[a.length -1]);
 
-// Page 152 exercise 6 in progress
+// Page 152 exercise 6 in progress --> when it is this.toString it works but the a[a.length -1] doesn't works. 
+// When there is return arg then a[a.length -1] works but toString dosnt works (it returns whole function) --> fix toString
