12612adaae0459b466fd53045d4f2876100bd262
Marcin Dolecki <marcin.dolecki@kramphub.com>
2023-01-05 01:13:50 +0100

    Object - exercise 6 in progress push

--- test.js
+++ test.js
@@ -14,27 +14,37 @@ function MyArray(...args) {
     console.log(arguments);
     var arg = arguments;
 
-    var b;
+    // conversion to string
+    var str = "";
+    for (var a in arg) {
+        if (a < arg.length -1) {
+            str += arg[a] + ',';
+        } else {
+            str += arg[a];
+        }
+    }
 
     // toString
     arg.toStringMy = function() {
-        function f() {
-            var str = "";
-            for (var a in arg) {
-                if (a < arg.length -1) {
-                    str += arg[a] + ',';
-                } else {
-                    str += arg[a];
-                }
-            }
-            return arg.str;
-        }
-        f();
+        return str;
     }
 
-    // // length
-    // this.lengthMy = arg.length;
+    // length
+    arg.lengthMy = arg.length;
 
+    var r = {};
+
+    // push
+    arg.pushMy = function(...args) {
+        var p = arguments;
+        var i = arg.length;
+        var j = 0;
+        for (i; i < arg.length + p.length; i++) {
+            arg[i] = p[j];
+            j++
+        }
+        return arg;
+    }
 
 
     // return
@@ -43,9 +53,9 @@ function MyArray(...args) {
 
 var a = new MyArray(1,2,3,"test");
 console.log(a.toStringMy());
-// console.log(a.lengthMy);
-// console.log(a);
+console.log(a.lengthMy);
 console.log(a[a.length -1]);
+console.log(a.pushMy('hey', 99));
+console.log(a.toStringMy());
 
-// Page 152 exercise 6 in progress --> when it is this.toString it works but the a[a.length -1] doesn't works. 
-// When there is return arg then a[a.length -1] works but toString dosnt works (it returns whole function) --> fix toString
+// Page 152 exercise 6 in progress --> find out how to overwrite global object from function.
