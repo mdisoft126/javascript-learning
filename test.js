3cf1e78985387284d52f5f45c15a75d336e363d6
Marcin Dolecki <mdisoft126@gmail.com>
2023-01-11 23:21:34 +0100

    Object - exercise 7 finished

--- test.js
+++ test.js
@@ -9,9 +9,16 @@
 
 //Exercise 6
 
+
+
 function MyMath() {
 
-    this.rand = function(min, max) {
+    // set type of parameter
+    /**
+    * @param {Boolean} incl - The boolean
+    */
+
+    this.rand = function(min, max, incl) {
 
         // date
         const date = new Date();
@@ -20,22 +27,35 @@ function MyMath() {
         // random number
         var r = parseInt(t.slice(-1));
 
-        // factor
+        // main
         var f;
         var res;
         var diff = max - min;
-        if (r === 0) {
-            res = parseInt((diff * 0) + min);
+        if (incl === true) {
+            if (r === 0) {
+                res = parseInt((diff * 0) + min);
+            } else {
+                f = (r/10);
+                res = parseInt(((diff * f) + min +1));
+            }
+            return res;
         } else {
-            f = (r/10);
-            res = parseInt(((diff * f) + min));
+            if (r === 0) {
+                res = parseInt((diff * 0) + min + 1);
+            } else if (r === 1) {
+                f = (r/10);
+                res = parseInt(((diff * f) + min + 1));
+            } else {
+                f = (r/10);
+                res = parseInt(((diff * f) + min));
+            }
+            return res;
         }
-        return res;
     }
 }
 
 var a = new MyMath();
-console.log(a.rand(5,10));
+console.log(a.rand(5, 10, true));
 
 
 // Page 152 exercise 7 in progress --> add functionality for included max
