0d6596d3d81fc5cb7de840eeaaf22f9ad7887f2d
Marcin Dolecki <mdisoft126@gmail.com>
2023-01-12 23:59:15 +0100

    Prototypes - adding fields and methods in progress - exe from W3S

--- test.js
+++ test.js
@@ -9,22 +9,46 @@
 
 // 5 Prototypes
 
-function Gadget(name, color) {
+// function Gadget(name, color) {
+//     this.name = name;
+//     this.color = color;
+//     this.whoAreYou = function() {
+//         return "I am " + this.color + ' ' + this.name;
+//     }
+//     Gadget.prototype.price = 100;
+//     Gadget.prototype.user_rating = 3;
+//     Gadget.prototype.inform = function() {
+//         return "User rating: " + this.user_rating + ", price: " + this.price;
+//     }
+
+//     // Gadget.prototype = {
+//     //     price: 100,
+//     //     user_rating: 3,
+//     //     inform: function() {
+//     //         return "User rating: " + this.user_rating + " price: " + this.price;
+//     //     }
+//     // }
+// }
+
+// var a = new Gadget("Henry", "Yellow");
+// console.log(a.whoAreYou());
+// console.log(typeof Gadget());
+
+// exercise from W3Schools
+
+function Person(name, surname, age) {
     this.name = name;
-    this.color = color;
-    this.whoAreYou = function() {
-        return "I am " + this.color + ' ' + this.name;
-    }
-    Gadget.prototype.price = 100;
-    Gadget.prototype.user_rating = 3;
-    Gadget.prototype.inform = function() {
-        return "User rating: " + this.user_rating + ", price: " + this.price;
-    }
+    this.surname = surname;
+    this.age = age;
 }
 
-var a = new Gadget("Henry", "Yellow");
-console.log(a.whoAreYou());
-console.log(a.inform());
+var a = new Person("Mark", "Shoolz", 35);
+console.log(a.name);
+
+Person.prototype.nationality = "German";
+console.log(a.nationality);
+
+console.log(a);
 
 
 // Page 157 --> finish at the begining of the page;
