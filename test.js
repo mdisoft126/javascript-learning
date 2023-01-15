10a8129e59f22e8bca55494637d3bb2f3234b040
Marcin Dolecki <mdisoft126@gmail.com>
2023-01-15 01:23:45 +0100

    Prototypes - adding fields and methods done

--- test.js
+++ test.js
@@ -9,6 +9,7 @@
 
 // 5 Prototypes
 
+//// 1st way
 // function Gadget(name, color) {
 //     this.name = name;
 //     this.color = color;
@@ -20,54 +21,31 @@
 //     Gadget.prototype.inform = function() {
 //         return "User rating: " + this.user_rating + ", price: " + this.price;
 //     }
-
-//     // Gadget.prototype = {
-//     //     price: 100,
-//     //     user_rating: 3,
-//     //     inform: function() {
-//     //         return "User rating: " + this.user_rating + " price: " + this.price;
-//     //     }
-//     // }
 // }
 
-// var a = new Gadget("Henry", "Yellow");
-// console.log(a.whoAreYou());
-// console.log(typeof Gadget());
-/////////////////////
-
-// exercise from mozilla
-
-var personPrototype = {
-    greet() {
-        // console.log(`Hello ${this.name}!`);
-        return `Hello ${this.name}!`;
+//// 2nd way
+function Gadget(name, color) {
+    this.name = name;
+    this.color = color;
+    this.whoAreYou = function() {
+        return "I am " + this.color + ' ' + this.name;
     }
 }
 
-function Person(name) {
-    this.name = name;
+Gadget.prototype = {
+    price: 100,
+    user_rating: 3,
+    inform: function() {
+        return "User rating: " + this.user_rating + " price: " + this.price;
+    }
 }
 
-Object.assign(Person.prototype, personPrototype);
-// or
-// Person.prototype.greet = personPrototype.greet;
 
-var a = new Person('Macin');
-console.log(a.name);
-console.log(a.greet());
-// console.log(Object.hasOwn(a, "name"));
-
-//// Object.hasOwn is valid from Node version 16. Here is 10
-// console.log(Object.hasOwn(a, "name"));
-// console.log(Object.hasOwn(a, "greet"));
-
-console.log(a.hasOwnProperty("name"));
-console.log(a.hasOwnProperty("greet"));
-
-
-// Finish prototypes from Mozilla page !!!
-// https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Objects/Object_prototypes
-// next header --> Prototypes and inheritance
+var a = new Gadget("Henry", "Yellow");
+console.log(a.whoAreYou());
+console.log(a.price);
+console.log(a.inform());
+console.log(typeof a);
 
 
 // Page 157 --> finish at the begining of the page;
