3e285fb942608ebd5f18e642102d22a0379a11c4
Marcin Dolecki <mdisoft126@gmail.com>
2023-01-17 21:47:32 +0100

    Prototypes - isPrototypeOf()

--- test.js
+++ test.js
@@ -9,47 +9,24 @@
 
 // 5 Prototypes
 
-function Gadget(name, color) {
+var monkey = {
+    eat: 'bananas',
+    hairy: true
+}
+
+function Human(name) {
     this.name = name;
-    this.color = color;
-    this.method = function() {
-        return "Hello " + this.color + " " + this.name;
-    }
-}
-Gadget.prototype.price = 100;
-Gadget.prototype.user_rating = 4;
-
-var a = new Gadget("John", "yellow");
-console.log(a);
-
-for (var field in a) {
-    console.log(field + " = " + a[field]);
 }
 
-console.log(a.hasOwnProperty('name'));
-console.log(a.hasOwnProperty('price') + '\n');
+Human.prototype = monkey;
 
-// only for own ones
-
-for (var own in a) {
-    if (a.hasOwnProperty(own)) {
-        console.log(own + " = " + a[own]);
-    }
-}
-
+var a = new Human("John");
+console.log(a.name);
+console.log(a.eat);
+console.log(a.hairy);
 console.log('\n');
 
-// is enumerable
-
-console.log(a.propertyIsEnumerable('name'));
-console.log(a.propertyIsEnumerable('method'));
-console.log(a.propertyIsEnumerable('price'));
-console.log(a.propertyIsEnumerable('constructor'));
-
-console.log('\n');
-
-console.log(a.constructor.prototype.propertyIsEnumerable('price'));
-
+console.log(monkey.isPrototypeOf(a));
 
 
 // Page 162 --> next "isPrototypeOf()";
