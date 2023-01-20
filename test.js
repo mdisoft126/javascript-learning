375b9a2706b679e609019a0bbad1c230cda5eadf
Marcin Dolecki <mdisoft126@gmail.com>
2023-01-20 01:00:58 +0100

    Prototypes - prototype pitfalls - done

--- test.js
+++ test.js
@@ -11,13 +11,49 @@
 
 //// check if the the field or method which you want to add to the build-in method, doesn't already exists.
 
-if (!String.prototype.reverse) {
-    String.prototype.reverse = function() {
-        return Array.prototype.reverse.apply(this.split('')).join('');
-    }
+function Dog() {
+    this.tail = true;
 }
 
-var s = 'abc';
-console.log(s.reverse());
+var pimpek = new Dog;
+var max = new Dog;
 
-// Page 167 --> prototype pitfalls;
+console.log(pimpek.tail);
+console.log(max.tail);
+
+console.log(pimpek.constructor);
+console.log(pimpek.constructor.prototype.constructor);
+
+console.log(typeof pimpek.tail);
+console.log(typeof pimpek.constructor.prototype.tail);
+
+Dog.prototype.bark = function() {
+    return 'Hau hau!';
+}
+
+console.log(pimpek.bark());
+
+Dog.prototype.legs = 4;
+
+console.log(pimpek.legs);
+
+Dog.prototype = {big: true, color: 'white'};
+// Dog.prototype.constructor = Dog; // see what this line change --> needed to working correctly!!!!!!!!!!
+
+
+console.log(pimpek.color); // undefined --> didn't overwrited the old one
+console.log(pimpek.bark());
+
+var donkey = new Dog;
+
+console.log(donkey.color);
+// console.log(donkey.bark()); // didn't work as the new onject use the new prototype
+
+console.log(donkey.constructor);
+console.log(pimpek.constructor);
+
+console.log(typeof pimpek.constructor.prototype.color);
+console.log(typeof donkey.constructor.prototype.color);
+
+
+// Page 169 --> next summary;
