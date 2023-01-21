ae9b6fa427356f41b85c76e56e0fab4a47c9c457
Marcin Dolecki <mdisoft126@gmail.com>
2023-01-21 01:24:18 +0100

    Prototypes - exercise 2, 3, 4, 5

--- test.js
+++ test.js
@@ -7,17 +7,50 @@
 
 ////
 
-// 5 Prototypes - Exercise 1
+// 5 Prototypes - Exercise 2, 3, 4, 5
 
+// prototype for Triangle
 var shape = {
-    type: 'square',
+    type: 'triangle',
     getType: function() {
         return this.type;
     }
 }
 
-console.log(shape.type);
-console.log(shape.getType());
+// Constructor
+function Triangle(a, b, c) {
+    this.a = a;
+    this.b = b;
+    this.c = c;
+}
+
+// assign prototype shape to Triangle
+Triangle.prototype = shape; 
+
+// add a new method to prototype
+shape.getCircuit = function() {
+    return this.a + this.b + this.c;
+}
+
+var t = new Triangle(1, 2, 3);
+console.log(t.a);
+console.log(t.b);
+console.log(t.c);
+console.log(t.type);
+console.log(t.getType());
+console.log(t.getCircuit());
+console.log(t.constructor);
+console.log(t.constructor.prototype.constructor);
+console.log(shape.isPrototypeOf(t));
+console.log(t.own);
+
+// has own property
+for (var f in t) {
+    if (t.hasOwnProperty(f)) {
+        console.log(f);
+    }
+}
+
 
 
 // Page 170 --> exercises;
