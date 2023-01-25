e2697f1b8001a5bfdb0c40a7a40e5565cb2ab69a
Marcin Dolecki <mdisoft126@gmail.com>
2023-01-25 23:32:36 +0100

    Inheritance - Uber: access to parent object v1

--- test.js
+++ test.js
@@ -9,60 +9,50 @@
 
 // 6 Inheritance
 
-//////// Out of the book --> Question mark operator
-
-//// 1. Ternary operator
-
-var temp = 35;
-var tempState = temp > 30 ? "Warm" : "Cold";
-console.log(tempState);
-
-//// 2. Optional Chaining
-
-var user = {
-    name: "Alex",
-    age: 24,
-    tell: function() {
-        return 1000;
-    }
+//// Parent
+function Parent() {};
+Parent.prototype.name = 'parent';
+Parent.prototype.getName = function() {
+    return this.name;
+}
+Parent.prototype.getParentName = function() {
+    return this.constructor.uber.name;
 }
 
-// console.log(user.salary?.()); // expecting output: undefined //// working only since Node 14
+//// Child1
+function Child1() {};
+var F = function() {};
+F.prototype = Parent.prototype;
+Child1.prototype = new F();
+Child1.prototype.constructor = Child1;
 
-//// 3. Nullish Coalescing
+// Uber for Child1 --> creating connection to parent!!!
+Child1.uber = Parent.prototype;
 
-// option with or ||
+// extending prototype
+Child1.prototype.name = 'child1';
 
-var a = {
-    name: undefined || 'N/A',
-    color: undefined || 'Some description',
-    species: 'fish' || 'Unknown species'
-}
-console.log(a.name);
-console.log(a.color);
-console.log(a.species);
+//// Child2
+function Child2() {};
+var F = function() {};
+F.prototype = Child1.prototype;
+Child2.prototype = new F();
+Child2.prototype.constructor = Child2;
 
-// but
+// Uber for child2 --> creating connection to parent!!!
+Child2.uber = Child1.prototype;
 
-var b = {
-    name: '' || 'N/A',
-    age: 0 || 24
-}
+// extending prototype
+Child2.prototype.name = 'child2';
 
-console.log(b.name); // we expect '' but got 'N/A'
-console.log(b.age); // we expect 0 but got 24
+var a = new Child2();
+console.log(a.getName());
+console.log(a.getParentName());
 
-// so in that case better use ??
+var b = new Child1();
+console.log(b.getName());
+console.log(b.getParentName());
 
-var c = {
-    name: undefined ?? 'N/A',
-    color: '' ?? 'N/A',
-    age: 0 ?? 30
-}
-
-console.log(c.name); // expecting output: 'N/A' //// working only since Node 14
-console.log(c.color); // expecting output: '' //// working only since Node 14
-console.log(c.age); // expecting output: 0 //// working only since Node 14
 
 
 // Page 180 --> next Uber: access to the parent-object
