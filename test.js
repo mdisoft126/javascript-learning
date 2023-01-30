e716348929d93c8ea114207eb9b7bbaf103387b9
Marcin Dolecki <mdisoft126@gmail.com>
2023-01-31 00:52:45 +0100

    Inheritance - Objects inherit from objects

--- test.js
+++ test.js
@@ -9,46 +9,43 @@
 
 //// 6 Inheritance
 
-// Beware of copying by reference!
-
-function extend(Child, Parent) {
-    var c = Child.prototype;
-    var p = Parent.prototype;
-    for (var i in p) {
+// extend by copying object
+function extendCopy(p) {
+    var c = {};
+    for(var i in p) {
         c[i] = p[i];
     }
-    Child.uber = p;
+    c.uber = p;
+    return c;
 }
 
-function Animal() {};
-Animal.prototype.name = 'animal';
-Animal.prototype.table = [1,2,3];
-function Mammal() {};
-extend(Mammal, Animal);
+//// Create objects (no constructors)
+// grand parent
+var shape = {
+    name: 'shape',
+    toString: function() {return this.name}
+}
 
-console.log(Animal.prototype.name);
-console.log(Animal.prototype.table);
-console.log(Mammal.prototype.name);
-console.log(Mammal.prototype.table);
+// parent
+var shape2d = extendCopy(shape);
+shape2d.name = 'shape 2d';
+shape2d.toString = function() {return this.uber.toString() + ', ' + this.name};
+
+// child
+var triangle = extendCopy(shape2d);
+triangle.name = 'triangle';
+triangle.toString = function() {return this.uber.toString() + ', ' + this.name};
+triangle.side = 10;
+triangle.heght = 7;
+triangle.getArea = function() {return this.side * this.heght / 2};
+
+console.log(shape.name);
+console.log(shape2d.name);
+console.log(triangle.name);
 console.log('\n');
 
-// Simple type is not touched. Parent's name value is not touched
-Mammal.prototype.name = 'mammal';
-console.log(Animal.prototype.name);
-console.log(Mammal.prototype.name);
-console.log('\n');
+console.log(shape.toString());
+console.log(shape2d.toString());
+console.log(triangle.toString());
 
-// // Assignement to the new object is fine. Parent object will stay not touched.
-// Mammal.prototype.table = [3,4,5];
-// console.log(Animal.prototype.table);
-// console.log(Mammal.prototype.table);
-// console.log('\n');
-
-// Doing something on currecnt object like table will result that on the aparent object will be visible a new value as well.
-Mammal.prototype.table.push(3,4,5);
-console.log(Animal.prototype.table);
-console.log(Mammal.prototype.table);
-console.log('\n');
-
-
-// Page 186 next --> Objects inherit from objects
+// Page 187 next --> Deep copying
