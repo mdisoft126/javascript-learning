7995c735d8c76acddd054b8d24e7f94c574e4a23
Marcin Dolecki <mdisoft126@gmail.com>
2023-02-02 00:15:09 +0100

    Inheritance - Deeep copying - my example to understand

--- test.js
+++ test.js
@@ -10,11 +10,25 @@
 //// 6 Inheritance
 
 function deepCopy(p, c) {
-    var a = c || {};
-    var b = p;
-    return a;
+    var c = c || {};
+    for(var i in p) {
+        if (typeof p[i] === 'object') {
+            c[i] = (p[i].constructor === Array) ? [] : {};
+            deepCopy(p[i], c[i]);
+        } else {
+            c[i] = p[i];
+        }
+    }
+    return c;
 }
 
-console.log(deepCopy(22));
+var o = {
+    name: 'hello',
+    obj: {
+        field: 1,
+        desc: 'triangle'
+    }};
+
+console.log(deepCopy(o));
 
 // Page 187 next --> Deep copying- in progress
