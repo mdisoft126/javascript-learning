6dd8390c678b4085fa04f9752c4d87671e361826
Marcin Dolecki <mdisoft126@gmail.com>
2023-02-04 12:51:32 +0100

    Inheritance - borrowing a constructor - with prototype inheritance

--- test.js
+++ test.js
@@ -9,7 +9,7 @@
 
 //// 6 Inheritance
 
-//// Borrowing a constructor - without prototype inheritance
+//// Borrowing a constructor - with prototype inheritance
 
 // Shape constructor
 function Shape(id) {
@@ -23,6 +23,7 @@ Shape.prototype.toString = function() {return this.name};
 function Triangle() {
     Shape.apply(this, arguments);
 }
+Triangle.prototype = new Shape(); ///////// thanks for that, new object inherits prototype as well
 Triangle.prototype.name = 'triangle';
 
 var t = new Triangle(100);
@@ -30,6 +31,36 @@ console.log(t.id);
 console.log(t.name);
 console.log(t.size); // Shape prototype was not inherited
 console.log(t.toString()); // Shape prototype was not inherited
+console.log('\n');
+
+var a = new Shape(200);
+console.log(a.id);
+console.log(a.name);
+console.log(a.size); // Shape prototype was not inherited
+console.log(a.toString()); // Shape prototype was not inherited
+console.log('\n');
+
+var t = new Triangle(100);
+console.log(t.id);
+console.log(t.name);
+console.log(t.size); // Shape prototype was not inherited
+console.log(t.toString()); // Shape prototype was not inherited
+console.log('\n');
+
+console.log(t.name);
+console.log(t.__proto__.name);
+console.log(t.constructor.prototype.name);
+console.log('\n');
+
+console.log(delete t.name);
+// console.log(delete t.__proto__.name);
+// console.log(delete t.constructor.prototype.name);
+console.log('\n');
+
+console.log(t.name);
+console.log(t.__proto__.name);
+console.log(t.constructor.prototype.name);
+console.log('\n');
 
 
 // Page 196 next --> Borrowing a constructor - borrow a constructor and copy its prototype
