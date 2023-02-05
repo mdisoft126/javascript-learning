457d6a6edb9c4f07f16ee06773c3334230cdc5a2
Marcin Dolecki <mdisoft126@gmail.com>
2023-02-06 00:59:14 +0100

    Summary - 3. Temporary constructor - done

--- test.js
+++ test.js
@@ -14,28 +14,42 @@
 ////// 3. Temporary constructor
 // function tempConstructor
 function tempConstructor(Child, Parent) {
-    function F() {}; ////// --> remporary constructor
+    function F() {};
     F.prototype = Parent.prototype;
     Child.prototype = new F();
     Child.prototype.constructor = Child;
     Child.uber = Parent.prototype;
 }
 
-function Parent() {};
-Parent.prototype.name = 'parent';
-Parent.prototype.toString = function() {return this.name};
+// Parent
+function Shape() {
+    this.name = 'shape';
+    this.info = 'lololo'
+}
+Shape.prototype.size = 4;
+Shape.prototype.toString = function() {return this.name};
+Shape.prototype.tab = [1, 2, 3];
 
-function Child() {};
-Child.prototype.name = 'child'
+// Child
+function Shape2d() {
+    this.name = 'shape2d';
+}
 
-tempConstructor(Child, Parent);
+tempConstructor(Shape2d, Shape);
 
-var a = new Child();
-console.log(a.name);
-console.log(a.toString());
+var a = new Shape2d();
+console.log(a.name); // result: shape2d --> name from its own
+console.log(a.info); // reult: undefined --> this method copy only prototypes. So tere is no access to parent's own fields!!!!!!!
+console.log(a.size); // result: 4 --> value from paren't prototype
+console.log(a.tab);- // result: [1, 2, 3]
+console.log('\n');
 
+Shape2d.prototype.size = 6; ////// using this method we can't overwrite the simple parent's value
+Shape2d.prototype.tab.push(4, 5, 6); ////// adding new values to the tab field
 
-//////  /////////////////////////// finish this one!!!!!!!!!!!!!!!!!!!!!!!
+var b = new Shape();
+console.log(b.size); // result: 4 --> it is not overwritten because size is a simple type of value
+console.log(b.tab); // result: [1, 2, 3, 4, 5, 6] --> it was overwritten because for objects and tables there is a reference to the parten't value
 
 
 // Page 198 next --> Copying prototype fields 
