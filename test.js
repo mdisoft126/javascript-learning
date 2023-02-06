15f58f0a3fb097043d3c481c9ce102e37e78b3ad
Marcin Dolecki <mdisoft126@gmail.com>
2023-02-06 21:57:07 +0100

    Summary - 6. Deep copying

--- test.js
+++ test.js
@@ -11,51 +11,42 @@
 
 //// Summary
 
-////// 5. Copying all fields (shallow)
-// function shallowCopy
-function shallowCopy(p) {
-    var c = {};
+////// 6. Deep copying
+// function deepCopying
+function deepCopying(p, c) {
+    var c = c || {};
     for (var i in p) {
-        c[i] = p[i];
+        if (typeof p[i] === 'object') {
+            c[i] = (p[i].constructor === Array) ? [] : {};
+            deepCopying(p[i], c[i]);
+        } else {
+            c[i] = p[i];
+        }
     }
-    c.uber = p;
     return c;
 }
 
-// grand parent object
+// parent
 var shape = {
     name: 'shape',
-    toString: function() {return this.name}
+    tab: [1, 2, 3],
+    toString: function() {return this.name},
 }
 
-// parent object
-var shape2d = shallowCopy(shape);
-shape2d.name = 'shape2d';
-shape2d.toString = function() {return this.uber.toString() + ', ' + this.name};
-shape2d.tab = [1, 2, 3];
+// child
+var triangle = deepCopying(shape);
+console.log(triangle.name = 'triangle');
+triangle.tab.push(4, 5, 6);
 
-// child object
-var triangle = shallowCopy(shape2d);
-triangle.name = 'triangle';
-triangle.side = 4;
-triangle.height = 8;
-triangle.getArea = function() {return this.side * this.height / 2};
-
-//// call
+// call
 console.log(triangle.name);
 console.log(triangle.toString());
-console.log(triangle.getArea(4, 8));
 console.log(triangle.tab);
 console.log('\n');
 
-// overvriting parent's value
-triangle.tab.push(4, 5, 6);
-console.log(triangle.tab);
-console.log('\n');
-
-// checking parent table value
-console.log(shape2d.tab); // result: [1, 2, 3, 4, 5, 6] --> the partent't value was ooverwritten. it is a shallow copy then.
+// // check if parent was overwritten
+console.log(shape.tab); // result: [1, 2, 3] --> parent's value was not overwritten
 
 
 
-// Page 198 next --> 6. Deep copying
+// Page 198 next --> 7. Prototype inheritance
