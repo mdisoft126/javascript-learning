50cc48598f273ae56a5fe62511c85d97d32551c2
Marcin Dolecki <mdisoft126@gmail.com>
2023-02-12 23:38:28 +0100

    Inheritance - case study: we draw shapes - in progress

--- test.js
+++ test.js
@@ -9,45 +9,23 @@
 
 //// Summary
 
-////// 12. Borrowing a constructor and copying his prototype
-// function copyPrototypes()
-function copyPrototypes(Child, Parent) {
-    var c = Child.prototype;
-    var p = Parent.prototype;
-    for(var i in p) {
-        c[i] = p[i];
-    }
-    c.uber = p;
+////// Case study: we draw shapes
+//// Based on the points on coordinate axis there will be drawn shape (using canvas library)
+
+// Constructor Point --> set points needed later to create a shape
+function Point(x, y) {
+    this.x = x;
+    this.y = y;
 }
 
-// constructor Shape
-function Shape(id) {
-    this.id = id;
+// Constructor Line --> calculation of discance between points (a2 + b2 = c2)
+function Line(p1, p2) {
+    this.p1 = p1;
+    this.p2 = p2;
+    this.length = Math.sqrt(Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p1.y, 2));
 }
-Shape.prototype.name = 'shape';
-Shape.prototype.toString = function() {return this.name};
 
-// constructor Triangle
-function Triangle() {
-    Shape.apply(this, arguments);
-}
-copyPrototypes(Triangle, Shape);
-Triangle.prototype.name = 'triangle';
-
-// call
-var a = new Triangle();
-console.log(a.id);
-console.log(a.name);
-console.log(a.toString());
-
-// check if paren't value overwritten
-console.log(Shape.prototype.name);  // not, it is not overwritten
-
-// check if double inheritance
-console.log(typeof a.__proto__.id); // no, there is no double inheritance
-
-// additionally tere is an access to parent's value by uber
-console.log(a.uber.name);
+//
 
 
-// Page 200 next --> Case study: we draw shapes
+// Page 202 next --> Case study: we draw shapes --> Shaoe constructor
