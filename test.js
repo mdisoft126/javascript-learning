72aa072448d1ed5f6a10fb572aa9ee506433d86d
Marcin Dolecki <mdisoft126@gmail.com>
2023-03-25 22:50:42 +0100

    ES6 Iterators and Generators - Collections - Map

--- test.js
+++ test.js
@@ -11,18 +11,50 @@
 
 ////// ES6 Iterators and Generators
 //// Iterators and iterables
-// Generators
+// Collections
 
-// Iterating over generators
+// Map
 
-// use destructuring syntax
-function* logger() {
-    yield 'a'
-    yield 'b'
-}
+//create simple map
+const m = new Map();
+m.set("first", 1);
+console.log(m.get("first"));
 
-var [x,y,z] = logger();
-console.log(x,y,z);
+// other examples of map
+console.log(m.has("first"));
+m.delete("first");
+console.log(m.has("first"));
 
+m.set("foo", 1);
+m.set("bar", 2);
+console.log(m.size);
+
+m.clear();
+console.log(m.size);
+
+// creating by using iterable pairs
+const m2 = new Map([
+    [1, "hey"],
+    [2, "co"],
+    [3, "tam?"]
+]);
+console.log(m2);
+console.log(m2.get(1));
+console.log(m2.get(2));
+console.log(m2.get(3));
+
+// creating by chain
+const m3 = new Map().set(1, "hhjd").set(2, "hhgfds").set(3, "whgd");
+console.log(m3);
+
+// can include any type of key, even objec
+var obj = {};
+const m4 = new Map([
+    [1, "hey"],
+    [2, "co"],
+    [obj, "tam?"]
+]);
+console.log(m4);
+console.log(m4.has(obj));
 
 // Next // Collections 195
