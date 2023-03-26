a9374f36163f6945f843a41d77aeb11a490a0882
Marcin Dolecki <mdisoft126@gmail.com>
2023-03-27 00:25:24 +0200

    Classes and Modules

--- test.js
+++ test.js
@@ -9,27 +9,80 @@
 
 ////////// Chapter 5 from new book and then continue from 8
 
-////// ES6 Iterators and Generators
-//// Iterators and iterables
-// Collections
+////// Classes and Modules
+//// 
 
-// Set
-// Converting maps to arrays
-var s = new Set();
-s.add("one");
-console.log(s);
-console.log(s.has("one"));
-s.add("two");
-console.log(s);
-s.add("one")
-console.log(s);
-s.delete("one");
-console.log(s);
+// using proptotypes
+var Person = function(firstname) {
+    if(!(this instanceof Person)) {
+        throw new Error("Person is a contructor");
+    }
+    this.firstname = firstname;
+}
 
-var c = new Set([
-    'red', "blue", "yellow", "red", "green", "orange", "blue"
-]);
-console.log(c);
+Person.prototype.giveBirth = function() {
+    // .. code for giving birth
+}
+
+var Employee = function(firstname, lastname, job) {
+    if(!(this instanceof Employee)) {
+        throw new Error("Employee is a contructor");
+    }
+    Person.call(this, firstname);
+    this.lastname = lastname;
+    this.job = job;
+}
+
+Employee.prototype = Object.create(Person.prototype);
+Employee.prototype.constructor = Employee;
+Employee.prototype.startJob = function() {
+    // .. code for employee start job
+}
+
+var Engineer = function(firstname, lastname, job, department) {
+    if(!(this instanceof Engineer)) {
+        throw new Error("Engineer is a contructor");
+    }
+    Engineer.call(this, firstname, lastname, job);
+    this.department = department;
+}
+
+Engineer.prototype = Object.create(Employee.prototype);
+Engineer.prototype.constructor = Engineer;
+Engineer.prototype.startWorking = function() {
+    // .. code for start working
+}
+
+// using ES6 classes syntax
+class Person {
+    constructor(firstname) {
+        this.firstname = firstname;
+    }
+    giveBirth() {
+        // .. a person is born
+    }
+}
+
+class Employee extends Person {
+    constructor(firstname, lastname, job) {
+        super(firstname);
+        this.lastname = lastname;
+        this.job = job;
+    }
+    startJob() {
+        // .. an empoyee starts job
+    }
+}
+
+class Engineer extends Employee {
+    constructor(firstname, lastname, job, department) {
+        super(firstname, lastname, job);
+        this.department = department;
+    }
+    startWorking() {
+        // .. an engineer starts working
+    }
+}
 
 
 // Next Chaper 8 - Classes and modules page 262
