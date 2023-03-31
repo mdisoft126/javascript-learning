a13ae917afb7347223ac493ace7b8a5beeecc7ca
Marcin Dolecki <mdisoft126@gmail.com>
2023-03-31 14:23:42 +0200

    Promises and proxies - promise

--- test.js
+++ test.js
@@ -9,7 +9,7 @@
 
 ////////// Chapter 9 Promises and proxies
 
-////// javascript call stack
+////// callback
 // array with numbers
 const myNumbers = [1,4,-2,5,4,-10,-7,3];
 
@@ -27,7 +27,32 @@ function removeNeg(numbers, callback) {
 // calling removing function
 removeNeg(myNumbers, (x) => x >= 0);
 
+////// propmise // the same exercise but using promise
+// array with numbers
+const myNumbers2 = [-4,-6,-7];
 
+// removing function
+function myRemovingPromise(numbers) {
+    return new Promise(
+        function (resolve, reject) {
+            const myArray = [];
+            for (const x of numbers) {
+                if (x >= 0) {
+                    myArray.push(x);
+                }
+            }
+            if (myArray.length > 0) {
+                resolve(myArray)
+            } else {
+                reject("No values above or equal 0");
+            }
+        }
+    )
+}
 
+myRemovingPromise(myNumbers2).then(
+    function(value) {console.log(value);},
+    function(error) {console.log(error);}
+);
 
 // Next Chaper 10 - The browser environment, page 294
