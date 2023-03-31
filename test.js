4539c819a7e049c4d6ff643ff5ebddf85dde0dd4
Marcin Dolecki <mdisoft126@gmail.com>
2023-04-01 01:04:52 +0200

    Promises and proxies - proxy - function traps

--- test.js
+++ test.js
@@ -9,50 +9,30 @@
 
 ////////// Chapter 9 Promises and proxies
 ////// Proxy
-//// Set
-// object
-const obj = {
-    foo: 'bar',
-    baz: 'qux'
-};
+//// Function traps
+var car = {
+    name: "Ford",
+    method_1: function(text) {
+        console.log("Method_1 called with " + text);
+    }
+}
 
-// 1. Preventing certain properties from being set
-const proxy = new Proxy(obj, {
-    set(target, property, value) {
-        if (property === 'baz') {
-            throw new Error('Setting "baz" is not allowed.');
+var methodInterceptorProxy = new Proxy(car, {
+    get: function(target, propKey, receiver) {
+        // I only want to intercept mathod calls, not property access
+        var propValue = target[propKey];
+        if (typeof propValue != 'function') {
+            return propValue;
+        } else {
+            return function() {
+                console.log("intercepting call to " + propKey + " in car " + target.name);
+                // target is the object being proxied
+                return propValue.apply(target, arguments);
+            }
         }
-        target[property] = value;
     }
 });
 
-proxy.foo = 'hello';
-// proxy.baz = 'yoy'; // error
-console.log(obj);
-
-// 2. Logging property changes
-const proxy2 = new Proxy(obj, {
-    set(target, property, value) {
-        console.log(`Setting ${property} to ${value}`);
-        target[property] = value
-    }
-})
-
-proxy2.foo = 'hej hello';
-console.log(obj);
-
-// 3.Preventing new properties from being added
-const proxy3 = new Proxy(obj, {
-    set(target, property, value) {
-        if (!(property in target)) {
-            throw new Error("Can't add new property. You can only change values on the existing ones.")
-        }
-        target[property] = value;
-    }
-})
-
-proxy3.foo = "helo elo hey";
-// proxy3.age = 10; // error
-console.log(obj);
+methodInterceptorProxy.method_1("Mercedes");
 
 // Next Chaper 10 - The browser environment, page 294
