6ed298ca6efca95605d07d8975805e86de6b36b0
Marcin Dolecki <mdisoft126@gmail.com>
2023-04-14 00:28:41 +0200

    Coding and Design Patterns - Coding patterns - Namespaces

--- test.js
+++ test.js
@@ -7,10 +7,22 @@
 
 ////
 
-////////// Chapter 10 The browser environment
-////// DOM
+////////// Chapter 11 Coding and Design Patterns
+////// Coding patterns
+//// Namespaces
 
+// var MyVariables = "hello"; // commented for testing purpose
+// console.log(MyVariables);
 
-//
+// if the variable exists before then it is not overwrited. But if no, there is created object.
+var MyVariables = MyVariables || {};
 
-// Next Chaper 11 - Coding and design patterns 357
+MyVariables.name = "Marcin";
+MyVariables.myFunction = function() {
+    console.log("say hello!");
+}
+
+console.log(MyVariables.name);
+MyVariables.myFunction();
+
+// Next
