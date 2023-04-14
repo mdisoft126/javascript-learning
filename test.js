18fa2efc11520155793206739456eadafa844920
Marcin Dolecki <mdisoft126@gmail.com>
2023-04-15 00:46:47 +0200

    Coding and Design Patterns - Coding patterns - Lazy definition

--- test.js
+++ test.js
@@ -9,50 +9,7 @@
 
 ////////// Chapter 11 Coding and Design Patterns
 ////// Coding patterns
-//// Namespaced constructors
-
-// function for creating elements in the document
-// var MYAPP = MYAPP || {};                             // version for browser environment
-// MYAPP.dom = {};
-// MYAPP.dom.Element = function (type, properties) {
-// 	var tmp = document.createElement(type);
-// 	for (var i in properties) {
-// 		if (properties.hasOwnProperty(i)) {
-// 			tmp.setAttribute(i, properties[i]);
-// 		}
-// 	}
-// 	return tmp;
-// };
-
-// function for creating elements in the document
-var MYAPP = MYAPP || {};                                // version for non browser environment. return string
-
-MYAPP.dom = {};
-
-MYAPP.dom.Element = function(type, properties) {
-  var tmp = '<' + type;
-
-  for (var i in properties) {
-    if (properties.hasOwnProperty(i)) {
-      tmp += ' ' + i + '="' + properties[i] + '"';
-    }
-  }
-
-  tmp += '>' + (properties.innerHTML || '') + '</' + type + '>';
-
-  return tmp;
-};
+//// Lazy definition
 
 
-// initialization
-var myElement = MYAPP.dom.Element('div', {
-    'class': 'my-class',
-    'data-id': '12345',
-    'innerHTML': 'Hello, world!'
-  });
-//   document.body.appendChild(myElement);          // for browser environment
-  
-  console.log(myElement);
-
-
-// Next A namespace() method page 363
+// Next Configuration object 366
