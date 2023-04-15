e968e3276c9de9b63cc66f507cd041630fefc26c
Marcin Dolecki <mdisoft126@gmail.com>
2023-04-16 01:12:12 +0200

    Coding and Design Patterns - Coding patterns - Private properties and methods

--- test.js
+++ test.js
@@ -9,41 +9,7 @@
 
 ////////// Chapter 11 Coding and Design Patterns
 ////// Coding patterns
-//// Lazy definition
+//// Private properties and methods
 
 
-<<<<<<< HEAD
-=======
-// function for creating elements in the document
-var MYAPP = MYAPP || {};                                // version for non browser environment. return string
-
-MYAPP.dom = {};
-
-MYAPP.dom.Element = function(type, properties) {
-  var tmp = '<' + type;
-
-  for (var i in properties) {
-    if (properties.hasOwnProperty(i)) {
-      tmp += ' ' + i + '="' + properties[i] + '"';
-    }
-  }
-
-  tmp += '>' + (properties.innerHTML || '') + '</' + type + '>';
-
-  return tmp;
-};
-
-
-// initialization
-var myElement = MYAPP.dom.Element('div', {
-    'class': 'my-class',
-    'data-id': '12345',
-    'innerHTML': 'Hello, world!'
-  });
-//   document.body.appendChild(myElement);          // for browser environment
-  
-  console.log(myElement);
-
-
->>>>>>> 9375c2740aa1e81216c84e5476ad89b7ec73ee59
-// Next Configuration object 366
+// Next Privileged methods 370
