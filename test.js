4d3b28a9f9e7572e4119b31a5e6a8eb986509b95
Marcin Dolecki <mdisoft126@gmail.com>
2023-04-22 23:50:26 +0200

    Coding and Design Patterns - Design patterns - singleton 2 - example constructor

--- test.js
+++ test.js
@@ -8,8 +8,21 @@
 ////
 
 ////////// Chapter 11 Coding and Design Patterns
-////// Coding patterns
-//// 
+////// Desing patterns
+//// Singleton 2 pattern
 
+// example constructor
+function Logger() {
+  this.log = function(message) {
+    console.log(message);
+  };
+}
+
+var my_log = new Logger();
+my_log.log('some event');
+// ... 1000 lines of code later in a different scope ...
+var other_log = new Logger();
+other_log.log('some new event');
+console.log(other_log === my_log); // true !!!! but it returns false for me----->>>>
 
 // Next Design patterns 377
