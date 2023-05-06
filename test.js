b2d58f32aedef93306fcb8465e7bfa2482295e5b
Marcin Dolecki <mdisoft126@gmail.com>
2023-05-06 23:01:25 +0200

    Chapter 12 Testing and Debugging - JavaScript debugging - Runtime exeptions - Chrome Developer Tools done

--- test.js
+++ test.js
@@ -12,6 +12,19 @@
 //// Runtime exeptions
 // Chrome Developer Tools
 
+function engageGear(gear) {
+    if (gear === "R") { console.log("Reversing"); }
+    if (gear === "D") { console.log("Driving"); }
+    if (gear === "N") { console.log("Neutral/Parking"); }
+    throw new Error("Invalid Gear State");
+}
+try {
+    engageGear("R"); //Reversing
+    engageGear("P"); //Invalid Gear State
+}
+catch (e) {
+    console.log(e.message);
+}
 
 
-// Next - Chrome Developer Tools 401
+// Next - Chapter 13 - Reactive promgraming and React 408
