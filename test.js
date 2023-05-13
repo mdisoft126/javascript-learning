939589aaf8daf3f9f6b1ddf7c0b5e613c7b25fc8
Marcin Dolecki <mdisoft126@gmail.com>
2023-05-14 00:06:08 +0200

    Appendix C: Built-in Objects - Object - ECMAScript 5 additions to objects v3

--- test.js
+++ test.js
@@ -11,42 +11,14 @@
 ////// Object
 //// ECMAScript 5 additions to objects
 
-// ES5 data descriptor and accessor description in more practical example
-// First code block - fixed value
-var person1 = {};
-Object.defineProperty(person1, "legs", {
-  value: 2,
-  writable: true,
-  configurable: true,
-  enumerable: true
-});
-
-// Set legs to 4
-person1.legs = 4;
-
-// Output the value of legs
-console.log(person1.legs); // 2 - fixed value is not overwritten
-
-// Second code block - custom logic
-var person2 = {};
-Object.defineProperty(person2, "legs", {
-  set: function(v) {
-    if (v >= 0 && v <= 4) {
-      this._legs = v;
-    }
-  },
-  get: function() {
-    return this._legs || 2;
-  },
-  configurable: true,
-  enumerable: true
-});
-
-// Set legs to 3
-person2.legs = 5;
-
-// Output the value of legs
-console.log(person2.legs); // 3 - custom logic is applied
+// ES5 descriptor (defining a nonmalleable property)
+var person = {};
+Object.defineProperty(person, 'heads', {value: 1});
+console.log(person.heads);
+console.log(person.heads = 2);
+console.log(person.heads);
+console.log(delete person.heads);
+console.log(person.heads);
 
 
 // Next - ECMAScript 5 additions to objects page 433
