254f7138c385eee10cd50d72588e28aeeba77dda
Marcin Dolecki <mdisoft126@gmail.com>
2023-05-14 01:03:27 +0200

    Appendix C: Built-in Objects - Object - ECMAScript 5 additions to objects - Object.create(obj, descr)

--- test.js
+++ test.js
@@ -11,30 +11,12 @@
 ////// Object
 //// ECMAScript 5 additions to objects
 
-////// Object.getPrototypeOf(obj)
-//// ES3 --> isPrototypeOf
+////// Object.create(obj, descr)
 
-//Create a constructor function
-function Person(name) {
-    this.name = name;
-}
-
-// create an object from the constructor function
-var person1 = new Person("John");
-
-// check if person1 is an istance of Person using is PrototypeOf()
-console.log(Person.prototype.isPrototypeOf(person1));
-
-//// ES5 --> getPrototypeOf
-
-//Create a constructor function --> it is already created above
-
-// create an object from the constructor function
-var person2 = new Person("Nick");
-
-// check if person1 is an istance of Person using is PrototypeOf()
-console.log(Object.getPrototypeOf(person2));
-console.log(Array.prototype);
+var parent = {hi: "hello"};
+var o = Object.create(parent, {prop: {value: 1}});
+console.log(o.hi);
+console.log(o.prop);
 
 
-// Next - ECMAScript 5 additions to objects page 433
+// Next - Object.getOwnPropertyDescriptor(obj, property) page 436
