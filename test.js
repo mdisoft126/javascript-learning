5ed85195304cfa9c989082adf380354fa13afdde
Marcin Dolecki <mdisoft126@gmail.com>
2023-05-21 00:53:42 +0200

    Appendix C: Built-in Objects - Array - ECMAScript 5 additions to Array - Array.prototype.some(callback, this_obj)

--- test.js
+++ test.js
@@ -9,18 +9,18 @@
 
 ////////// Appendix C: Built-in Objects
 ////// Array
-//// Array.prototype.every(callback, this_obj)
+//// Array.prototype.some(callback, this_obj)
 
 function hasEye(el) {
     return el.indexOf('a') !== -1;
 }
 
-var a = ["itsya", "bitsya", "spidera"];
+var a = ["itsy", "bitsy", "spidera"];
 
-console.log(a.every(hasEye));
+console.log(a.some(hasEye));
 
 /////
-a.every(function (e) {
+a.some(function (e) {
     console.log(e);
     return false;
 });
