a4d9cbcb236a4fae5f5cf92a5ad975f82fa4ed9d
Marcin Dolecki <mdisoft126@gmail.com>
2023-05-21 00:52:06 +0200

    Appendix C: Built-in Objects - Array - ECMAScript 5 additions to Array - Array.prototype.every(callback, this_obj)

--- test.js
+++ test.js
@@ -9,34 +9,21 @@
 
 ////////// Appendix C: Built-in Objects
 ////// Array
-//// TECMAScript 5 additions to Array
+//// Array.prototype.every(callback, this_obj)
 
-//// Logger
-
-var logger = console.log.bind(console);
-
-//// forEach
-// var car = ["audi", "ford", "volvo"];
-
-// car.forEach(logger);
-
-
-//// normal function
-carNames(logger);
-
-function carNames(logger) {
-    var car = ["audi", "ford", "volvo"];
-    var result = "";
-
-    for (var i = 0; i < car.length; i++) {
-        if (i === car.length-1) {
-            result += car[i];
-        } else {
-            result += car[i] + "\n";
-        }
-    }
-    // return result;
-    logger(result);
+function hasEye(el) {
+    return el.indexOf('a') !== -1;
 }
+
+var a = ["itsya", "bitsya", "spidera"];
+
+console.log(a.every(hasEye));
+
+/////
+a.every(function (e) {
+    console.log(e);
+    return false;
+});
+
 
 // Next ES6 addition to arrays 447
