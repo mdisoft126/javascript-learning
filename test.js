beb07765e40799623c7499eb784cc966047b69b0
Marcin Dolecki <mdisoft126@gmail.com>
2023-05-21 16:21:32 +0200

    Appendix C: Built-in Objects - Array - ECMAScript 5 additions to Array - Array.prototype.entries() Array.prototype.keys() Array.prototype.values()

--- test.js
+++ test.js
@@ -10,10 +10,25 @@
 ////////// Appendix C: Built-in Objects
 ////// Array
 //// ES6 addition to arrays
-//// Array.of(...items)
+//// Array.prototype.entries() Array.prototype.keys() Array.prototype.values()
 
-var arr = Array.of(1,2,3, "foo");
-console.log(arr);
+let a = Array.of(1,2,3,"foo");
 
+let k,v,e;
 
-// Next Array.prototype.entries() Array.prototype.keys() Array.prototype.values() 448
+console.log("// keys:");
+for (k of a.keys()) {
+    console.log(k);
+}
+
+console.log("// values:");
+for (v of a.values()) {
+    console.log(v);
+}
+
+console.log("// entries:");
+for (e of a.entries()) {
+    console.log(e);
+}
+
+// Next Array.prototype.find(predicate, thisArg?) 448
