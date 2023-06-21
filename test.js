e33d0c45c2ad0769cb20ce59a4c829fbd1988732
Marcin Dolecki <mdisoft126@gmail.com>
2023-06-21 07:39:18 +0200

    Appendix C: Built-in Objects - Function - ECMAScript 5 additions to a Function - bind - bound function

--- test.js
+++ test.js
@@ -26,5 +26,17 @@ console.log(unboundedGetX());
 const boundedGetX = myModule.getX.bind(myModule);
 console.log(boundedGetX());
 
+// creating new bound function
+function myLog(...args) {
+    console.log(this, ...args);
+}
+
+myLog(0, 0);
+const boundLog = myLog.bind("this value", 1,2);
+boundLog();
+const boundLog2 = boundLog.bind("this value 2", 3,4);
+boundLog2(5, 6);
+
+
 // Next Continue with bind example from the page:
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/bind?retiredLocale=pl
