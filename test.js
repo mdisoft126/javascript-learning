f01ae59b92bb49bc86cd2c495592b215d0ee2233
Marcin Dolecki <mdisoft126@gmail.com>
2023-06-30 08:21:19 +0200

    Appendix C: Built-in Objects - String - ECMAScript 6 additions to String

--- test.js
+++ test.js
@@ -9,17 +9,37 @@
 
 ////////// Appendix C: Built-in Objects
 ////// String
-//// ECMAScript 5 additions to String
+//// ECMAScript 6 additions to String
 
-// String.prototype.trim()
-console.log(" hello ");
-console.log(" hello ".trim());
-console.log(" \t hello \n ");
-console.log(" \t hello \n ".trim());
+// Template Literals
+var a = 3;
+var b = 5;
+console.log(`My number is ${a + b}.`);
+
+// String.prototype.repeat
+console.log("foo".repeat(3));
+var x = 5;
+console.log("a".repeat(2 * x));
+
+// String.prototype.startsWith
+console.log("hello".startsWith("ello", 0));
+console.log("hello".startsWith("ello", 1));
+console.log("hello".startsWith("ello", 2));
+
+// String.prototype.endsWith
+console.log("hello".endsWith("he",2));
+console.log("hello".endsWith("lo",5));
+console.log("hello".endsWith("ell",4));
+
+// String.prototype.includes
+console.log("hello".includes("l"));
+console.log("hello".includes("l", 0));
+console.log("hello".includes("l", 3));
+console.log("hello".includes("l", 4));
 
 // 
 
-// Next ECMAScript 6 additions to String page 459
+// Next Date page 459
 
 // Must be done after Command line and python!
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript
