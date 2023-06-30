06fb14eca603fc28e1f0e7d3a1ca1fd9a4dd12ed
Marcin Dolecki <mdisoft126@gmail.com>
2023-06-30 07:33:11 +0200

    Appendix C: Built-in Objects - String - ECMAScript 5 additions to String

--- test.js
+++ test.js
@@ -9,50 +9,17 @@
 
 ////////// Appendix C: Built-in Objects
 ////// String
-//// The String.prototype members
+//// ECMAScript 5 additions to String
 
-// replace(needle,replacement)
-console.log("R2-D2".replace(/2/g, "-two"));
-console.log("R22-D2".replace(/(2)/g, "$1$1"));
-console.log("R22-D2".replace(/(2)/g, "$1$1"));
-
-// regex example
-var myRegex = /(\w+)\s(\w+)/;
-var myString = "Hello World";
-var matches = myString.match(myRegex);
-console.log(matches);
-
-console.log(myString.replace(myRegex, "$1 $2"));
-console.log(myString.replace(myRegex, "$2 $1"));
-console.log(myString.replace(myRegex, "$2$1"));
-console.log(myString.replace(myRegex, "$2, $1"));
-
-// search(regexp)
-console.log("P-123".search(/[0-9]/));
-
-// slice(start, end)
-console.log("hello, how is life".slice(4, 13));
-console.log("hello, how is life".slice(4, -1));
-console.log("hello, how is life".slice(-7, -1));
-console.log("hello, how is life".slice(-4));
-
-// split(separator, limit)
-console.log("1,2,3,4".split(/,/));
-console.log("1,2,3,4".split(",", 2));
-console.log("1,2,3,4".split(/[0-9]/));
-
-// substring(start, end)
-console.log("hello, how is life".substring(4, 13));
-console.log("hello, how is life".substring(13, 4));
-console.log("hello, how is life".substring(-3, 13));
-console.log("hello, how is life".substring(3, -6));
-console.log("hello, how is life".substring(5));
-console.log("hello, how is life".substring(5, -7));
-console.log("hello, how is life".substring(5, 30));
+// String.prototype.trim()
+console.log(" hello ");
+console.log(" hello ".trim());
+console.log(" \t hello \n ");
+console.log(" \t hello \n ".trim());
 
 // 
 
-// Next ECMAScript 5 additions to String page 458
+// Next ECMAScript 6 additions to String page 459
 
 // Must be done after Command line and python!
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript
