a79f66835a044a85286566a70916a5645b404981
Marcin Dolecki <mdisoft126@gmail.com>
2023-07-31 08:28:04 +0200

    Appendix C: Built-in Objects - Date - The Date.prototype members

--- test.js
+++ test.js
@@ -9,19 +9,34 @@
 
 ////////// Appendix C: Built-in Objects
 ////// Date
-//// Members of the Date constructor
+//// The Date.prototype members
 
-// Date.parse(string)
-console.log(Date.parse('May 5, 2015'));
-console.log(Date.parse('03,04,2015'));
-console.log(Date.parse('04-03-2015'));
+// toUTCString()
+var a = new Date(2015, 0, 1);
+console.log(a.toString());
+console.log(a.toUTCString());
 
-// Date.UTC(year, month...)
-console.log(Date.UTC(2015,04,03));
+// toDateSting()
+console.log(a.toDateString());
+
+// toTimeString()
+console.log(a.toTimeString());
+
+// toLocale...
+console.log(a.toLocaleString());
+console.log(a.toDateString());
+console.log(a.toTimeString());
+
+// getTime() setTime(time)
+console.log(a.toLocaleString());
+console.log(a.getTime());
+console.log(a.setTime(a.getTime() + 1000 * 60 * 60 * 24));
+console.log(a.toLocaleString());
+console.log(a.getTime());
 
 // 
 
-// Next  page 460
+// Next The Date.prototype members page 462
 
 // Must be done after Command line and python!
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript
