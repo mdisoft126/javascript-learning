e57b2817c893a95c2d5268827dbeb221e5d83629
Marcin Dolecki <mdisoft126@gmail.com>
2023-08-01 08:41:00 +0200

    Appendix C: Built-in Objects - Date - The Date.prototype members part2

--- test.js
+++ test.js
@@ -11,32 +11,87 @@
 ////// Date
 //// The Date.prototype members
 
-// toUTCString()
-var a = new Date(2015, 0, 1);
-console.log(a.toString());
-console.log(a.toUTCString());
+// getYear()
+var d = new Date(2015,0,1);
+console.log(d);
+console.log(d.getYear());
+console.log(d.getFullYear());
+console.log(d.getUTCFullYear());
+console.log(d.setFullYear(2020));
+console.log(d);
+console.log(d.setUTCFullYear(2022));
+console.log(d);
 
-// toDateSting()
-console.log(a.toDateString());
+// getMonth()
+var d = new Date(2015,0,1);
+console.log(d);
+console.log(d.getMonth());
+console.log(d.getUTCMonth());
+console.log(d.setMonth(4));
+console.log(d);
+console.log(d.setUTCMonth(2));
+console.log(d);
+console.log(d.toLocaleDateString());
+console.log("testing");
+var d = new Date(Date.UTC(2020, 0, 1));
+console.log(d);
+console.log(d.setUTCMonth(4));
+console.log(d);
+console.log(d.toLocaleDateString());
+console.log(d.toDateString());
 
-// toTimeString()
-console.log(a.toTimeString());
+// getDate()
+console.log(d.getDate());
+console.log(d.getUTCDate());
+console.log(d.setDate(15));
+console.log(d);
+console.log(d.setUTCDate(20));
+console.log(d);
 
-// toLocale...
-console.log(a.toLocaleString());
-console.log(a.toDateString());
-console.log(a.toTimeString());
+// getHours, minutes...
+console.log(d.getHours());
+console.log(d.getUTCHours());
+console.log(d.setHours(8));
+console.log(d.getHours());
+console.log(d.getUTCHours());
+console.log(d.setUTCHours(10));
+console.log(d.getHours());
+console.log(d.getUTCHours());
+console.log(d.getMinutes());
+console.log(d.setMinutes(5));
+console.log(d);
+console.log(d.setUTCMinutes(10));
+console.log(d);
+console.log(d.setSeconds(20));
+console.log(d);
+console.log(d.setMilliseconds(999));
+console.log(d);
+console.log(d.setMilliseconds(1005));
+console.log(d);
 
-// getTime() setTime(time)
-console.log(a.toLocaleString());
-console.log(a.getTime());
-console.log(a.setTime(a.getTime() + 1000 * 60 * 60 * 24));
-console.log(a.toLocaleString());
-console.log(a.getTime());
+// getTimezoneOffset()
+console.log(new Date().getTimezoneOffset());
+
+// getDay()
+console.log(d.toDateString());
+console.log(d.toLocaleDateString());
+console.log(d.getDay());
+console.log(d.getUTCDay());
+
+var d = new Date(Date.UTC(2020, 0, 1));
+console.log(d);
+console.log(d.toDateString());
+console.log(d.toLocaleDateString());
+console.log(d.getDay());
+console.log(d.getUTCDay());
+console.log(d.setUTCHours(8));
+console.log(d);
+console.log(d.getUTCDay());
 
 // 
 
-// Next The Date.prototype members page 462
+
+// Next ECMAScript 5 additions to Date page 464
 
 // Must be done after Command line and python!
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript
