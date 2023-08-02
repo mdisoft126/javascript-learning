5322a31c8f3149d763dab12e3e1e3b46e1acf71c
Marcin Dolecki <mdisoft126@gmail.com>
2023-08-02 08:41:54 +0200

    Appendix C: Built-in Objects - Math - Members of the Math object

--- test.js
+++ test.js
@@ -9,18 +9,33 @@
 
 ////////// Appendix C: Built-in Objects
 ////// Math
-//// 
+//// Members of the Math object
 
+// some constants
+console.log(Math.E);
+console.log(Math.PI);
+console.log(Math.SQRT2);
+console.log(Math.SQRT1_2);
+console.log(Math.LN10);
+console.log(Math.LN2);
+console.log(Math.LOG10E);
+console.log(Math.LOG2E);
+
+// Trigonometric functions
+// sin vs asin explanation
+console.log("triginomic");
+
+const angleInRadians = Math.PI / 6; // 30 degrees in radians
+const sinValue = Math.sin(angleInRadians);
+console.log(sinValue);
+
+const sinValue2 = 0.5;
+const angleInRadians2 = Math.asin(sinValue2); // Returns Math.PI / 6 (30 degrees in radians)
+console.log(angleInRadians2);
 // 
-console.log(typeof Date.prototype);
-console.log(typeof Math.prototype);
-console.log(typeof String);
-console.log(typeof Math);
-
-// 
 
 
-// Next Math page 464
+// Next Trigonometric functions page 465
 
 // Must be done after Command line and python!
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript
