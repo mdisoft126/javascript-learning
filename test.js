99c3d21e25fc5fa8108745564467137a6bb09374
Marcin Dolecki <mdisoft126@gmail.com>
2023-08-02 08:00:39 +0200

    Appendix C: Built-in Objects - Date - ECMAScript 5 additions to Date

--- test.js
+++ test.js
@@ -9,89 +9,27 @@
 
 ////////// Appendix C: Built-in Objects
 ////// Date
-//// The Date.prototype members
+//// ECMAScript 5 additions to Date
 
-// getYear()
-var d = new Date(2015,0,1);
-console.log(d);
-console.log(d.getYear());
-console.log(d.getFullYear());
-console.log(d.getUTCFullYear());
-console.log(d.setFullYear(2020));
-console.log(d);
-console.log(d.setUTCFullYear(2022));
-console.log(d);
+// Date.now()
+console.log(Date.now() === new Date().getTime());
+console.log(Date.now());
+console.log(new Date().getTime());
 
-// getMonth()
-var d = new Date(2015,0,1);
-console.log(d);
-console.log(d.getMonth());
-console.log(d.getUTCMonth());
-console.log(d.setMonth(4));
-console.log(d);
-console.log(d.setUTCMonth(2));
-console.log(d);
-console.log(d.toLocaleDateString());
-console.log("testing");
-var d = new Date(Date.UTC(2020, 0, 1));
-console.log(d);
-console.log(d.setUTCMonth(4));
-console.log(d);
-console.log(d.toLocaleDateString());
-console.log(d.toDateString());
+// Date.prototype.toISOString()
+var d = new Date(2015, 0, 1);
+console.log(d.toString());
+console.log(d.toUTCString());
+console.log(d.toISOString());
 
-// getDate()
-console.log(d.getDate());
-console.log(d.getUTCDate());
-console.log(d.setDate(15));
-console.log(d);
-console.log(d.setUTCDate(20));
-console.log(d);
-
-// getHours, minutes...
-console.log(d.getHours());
-console.log(d.getUTCHours());
-console.log(d.setHours(8));
-console.log(d.getHours());
-console.log(d.getUTCHours());
-console.log(d.setUTCHours(10));
-console.log(d.getHours());
-console.log(d.getUTCHours());
-console.log(d.getMinutes());
-console.log(d.setMinutes(5));
-console.log(d);
-console.log(d.setUTCMinutes(10));
-console.log(d);
-console.log(d.setSeconds(20));
-console.log(d);
-console.log(d.setMilliseconds(999));
-console.log(d);
-console.log(d.setMilliseconds(1005));
-console.log(d);
-
-// getTimezoneOffset()
-console.log(new Date().getTimezoneOffset());
-
-// getDay()
-console.log(d.toDateString());
-console.log(d.toLocaleDateString());
-console.log(d.getDay());
-console.log(d.getUTCDay());
-
-var d = new Date(Date.UTC(2020, 0, 1));
-console.log(d);
-console.log(d.toDateString());
-console.log(d.toLocaleDateString());
-console.log(d.getDay());
-console.log(d.getUTCDay());
-console.log(d.setUTCHours(8));
-console.log(d);
-console.log(d.getUTCDay());
+// Date.prototype.toJSON()
+console.log(d.toJSON() === d.toISOString());
+console.log(d.toJSON());
 
 // 
 
 
-// Next ECMAScript 5 additions to Date page 464
+// Next Math page 464
 
 // Must be done after Command line and python!
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript
