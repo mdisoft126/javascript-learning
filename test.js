3a1d630d871961c8a11cdef5deb758d8e0f67372
Marcin Dolecki <mdisoft126@gmail.com>
2023-08-03 08:32:55 +0200

    Appendix C: Built-in Objects - RegExp - The RegExp.prototype members

--- test.js
+++ test.js
@@ -9,15 +9,40 @@
 
 ////////// Appendix C: Built-in Objects
 ////// RegExp
-////
+//// The RegExp.prototype members
 
-var re = new RegExp('[dn]o+dle', 'gmi');
+// global, ignoreCase, multiline
+
+var re = /[do]o+dle/gmi;
+
 console.log(re);
+console.log(re.global);
 
-var rel = ('/[dn]o+dle/gmi');
-console.log(rel);
+// last index
+console.log(re.lastIndex);
+console.log(re.exec("noodle doodle"));
+console.log(re);
+console.log(re.lastIndex);
+var result = re.exec("noodle doodle");
+console.log(result);
+console.log(re.exec("noodle doodle"));
+console.log(re.lastIndex);
 
-// Next page
+// source
+console.log(re.source);
+
+// exec
+var re2 = /([dn])(o+)dle/g;
+var re3 = /[dn]o+dle/g;
+console.log(re2.exec("noodle doodle"));
+console.log(re3.exec("noodle doodle"));
+
+// test
+console.log(re.test("noodle"));
+console.log(re.test("test"));
+
+
+// Next Error objects page 468
 
 // Must be done after Command line and python!
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript
