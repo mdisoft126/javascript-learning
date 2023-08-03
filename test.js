621035dd290868ea5326eade93a2c2bb9af08d54
Marcin Dolecki <mdisoft126@gmail.com>
2023-08-03 08:11:57 +0200

    Appendix C: Built-in Objects - RegExp

--- test.js
+++ test.js
@@ -8,47 +8,16 @@
 ////
 
 ////////// Appendix C: Built-in Objects
-////// Math
-//// Members of the Math object
+////// RegExp
+////
 
-// round
-var a = 5.4;
-console.log(Math.round(a));
-console.log(Math.ceil(a));
-console.log(Math.floor(a));
+var re = new RegExp('[dn]o+dle', 'gmi');
+console.log(re);
 
-// max min
-console.log(Math.max(1,52, Math.PI));
-console.log(Math.min(5, 60, Math.PI));
+var rel = ('/[dn]o+dle/gmi');
+console.log(rel);
 
-// abs
-console.log(Math.abs(-100));
-
-// exp
-console.log(Math.exp(1));
-
-// log
-console.log(Math.log(10));
-
-// sqrt
-console.log(Math.sqrt(9));
-
-// pow
-console.log(Math.pow(3,2));
-
-// random
-console.log(Math.random());
-
-// random 0-99
-console.log(Math.random()*100);
-
-// random 0-100
-console.log(Math.random()*101);
-// random 1-100
-console.log((Math.random()*100)+1);
-
-
-// Next RegExp page 466
+// Next page
 
 // Must be done after Command line and python!
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript
