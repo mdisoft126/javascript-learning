06ae0e85dda5fb815981e5fc0ab7ed6323cb7f40
Marcin Dolecki <mdisoft126@gmail.com>
2023-08-04 08:40:31 +0200

    Appendix C: Built-in Objects - JSON

--- test.js
+++ test.js
@@ -8,24 +8,44 @@
 ////
 
 ////////// Appendix C: Built-in Objects
-////// Error objects
-////
+////// JSON
+//// Members of the JSON object
 
-var e = new Error("error test");
-console.log(e);
-console.log(typeof e);
+// parse(text, callback)
 
-//// The Error.prototype members
-// name
-console.log(e.name);
+var data = '{"hello": 1, "hi": [1,2,3]}';
+console.log(data);
+console.log(typeof data);
+console.log(data.hello);
 
-// message
-console.log(e.message);
+var parsedData = JSON.parse(data);
+console.log(parsedData);
+console.log(typeof parsedData);
+console.log(parsedData.hello);
+console.log(parsedData.hi);
+
+console.log("//////////////");
+
+function callback(key, value) {
+    console.log(key, value);
+    if (key === 'hello') {
+        return 'bonjour';
+    }
+    if (key === 'hi') {
+        return undefined;
+    }
+    return value;
+}
+
+var o = JSON.parse(data, callback);
+console.log(o.hello);
+console.log(o.hi);
+console.log("hi" in o);
+console.log("hello" in o);
 
 
 
-
-// Next JSON page 469
+// Next Members of the JSON object page 471
 
 // Must be done after Command line and python!
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript
