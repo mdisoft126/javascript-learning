583d6673da75732f440d3309aa29f6520b33c53e
Marcin Dolecki <mdisoft126@gmail.com>
2023-08-12 15:02:40 +0200

    Appendix E: Answers to Exercise Questions - Chapter 3, Functions ex2

--- test.js
+++ test.js
@@ -9,67 +9,37 @@
 
 ////////// Appendix E: Answers to Exercise Questions
 ////// Chapter 3, Functions
-//// 1
+//// 2
 
-// getRGB("#00ff00")
-function getRGB(hex) {
-    return "rgb(" +
-        parseInt(hex[1] + hex[2], 16) + ", " +
-        parseInt(hex[3] + hex[4], 16) + ", " +
-        parseInt(hex[5] + hex[6], 16) + ")";
-} 
+// parseInt(1e1)
+console.log(1e1);
+console.log(2e1);
+console.log(2e0);
 
-console.log(getRGB("#00ff00"));
+console.log(parseInt(1e1));
+console.log(parseInt(2e1));
+console.log(parseInt(2e0));
 
-// getRGB2("#00ff00") - 2nd version
-function getRGB2(hex) {
-    hex = hex.replace("#", "");
-    var red = parseInt(hex.substring(0, 2), 16);
-    var green = parseInt(hex.substring(2, 4), 16);
-    var blue = parseInt(hex.substring(4, 6), 16);
+// parseInt("1e1")
+console.log("1e1");
+console.log(parseInt("1e1"));
 
-    return `RGB(${red}, ${green}, ${blue})`
-}
-
-console.log(getRGB2("#00ff00"));
-
-// substring vs slice
-var str = "Hello World!";
-console.log(str.substring(6, 11));
-console.log(str.substring(12, 6));
-console.log(str.substring(-3, 6));
+// parseFloat("1e1")
+console.log(parseFloat("1e1"));
+console.log(1e3);
+console.log(parseFloat("1e3"));
 console.log("--------");
 
-console.log(str.slice(6,11));
-console.log(str.slice(12,6));
-console.log(str.slice(-6,-1));
-console.log(str.slice(-1,-8));
-console.log(str.slice(-8,-1));
+// isFinite
+console.log(0/10);
+console.log(10/0);
+console.log(isFinite(0/10));
+console.log(isFinite(20/0));
 
-// getRGB3()
-function getRGB3(hex) {
-    var result = [];
-    result.push(parseInt(hex.slice(1,3),16));
-    result.push(parseInt(hex.slice(3,5),16));
-    result.push(parseInt(hex.slice(5),16));
-    return "rgb(" + result.join(", ") + ")";
-}
+// isNaN(parseInt(NaN))
+console.log(parseInt(NaN));
+console.log(isNaN(parseInt(NaN)));
 
-console.log(getRGB3("#00ff00"));
-
-// getRGB4()
-function getRGB4(hex) {
-    var result = [];
-    var i = 1, j = 3;
-    while (j < 8) {
-        result.push(parseInt(hex.slice(i,j),16));
-        i += 2;
-        j += 2;
-    }
-    return "rgb(" + result.join(", ") + ")";
-}
-
-console.log(getRGB4("#00ff00"));
 
 // Next Appendix E: Answers to Exercise Questions //
 
