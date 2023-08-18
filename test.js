38aca4658f66da237df9914d856ed670ce34d424
Marcin Dolecki <mdisoft126@gmail.com>
2023-08-18 08:35:38 +0200

    Appendix E: Answers to Exercise Questions - Chapter 4, Objects ex4

--- test.js
+++ test.js
@@ -9,18 +9,62 @@
 
 ////////// Appendix E: Answers to Exercise Questions
 ////// Chapter 4, Objects
-//// 3
+//// 4
 
 //
-var c = [1, 2, [1, 2]];
-console.log(c);
-console.log(c.join('--'));
-console.log(c.sort());
-console.log(c);
-console.log(c.join('--'));
+function MyString(input) {
+    var index = 0;
+
+    // cast to string
+    this._value = '' + input;
+
+    // access to array
+    // while (input[index] !== undefined) {
+    //     this[index] = input[index];
+    //     index++;
+    // }
+
+    while (this._value[index] !== undefined) {
+        this[index] = this._value[index];
+        index++;
+    }
+
+    // remember the length
+    this._length = index;
+
+}
+
+MyString.prototype = {
+    constructor: MyString,
+    valueOf: function valueOf() {
+        return this._value;
+    },
+    toString: function toString() {
+        return this.valueOf();
+    },
+    charAt: function charAt(index) {
+        // return this[parseInt(index, 10) || 0];
+        if (this[parseInt(index, 10)]) {
+            return this[parseInt(index, 10)];
+        }
+        else {
+            return "Wrong data type. Give a number";
+        }
+    }
+}
 
 
-// Next Appendix E: Answers to Exercise Questions //
+var a = new MyString(123);
+console.log(a._value);
+console.log(typeof a._value);
+console.log(a[1]);
+console.log(a._length);
+console.log(typeof a);
+console.log(typeof a.valueOf());
+console.log(typeof a.toString());
+console.log(a.charAt('l'));
+
+// Next Appendix E: Answers to Exercise Questions // page 487
 
 // Must be done after Command line and python!
 // https://developer.mozilla.org/en-US/docs/Web/JavaScrip
