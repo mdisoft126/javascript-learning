909b2860d1d4af099a8ba197a372f0f2db98df89
Marcin Dolecki <mdisoft126@gmail.com>
2023-09-12 08:26:02 +0200

    Appendix E: Answers to Exercise Questions - Chapter 4, Objects ex6 done

--- test.js
+++ test.js
@@ -28,12 +28,36 @@ function MyArray(length) {
 
 MyArray.prototype = {
     constructor: MyArray,
-    join: function _join(glue) {
+    _join: function _join(glue) {
         var result = '';
         if (glue === undefined) {
             glue = ',';
         }
-        
+        for (var i = 0; i < this.length -1; i++) {
+            result += this[i] === undefined ? '' : this[i];
+            result += glue;
+        }
+        result += this[i] === undefined ? '' : this[i];
+        return result;
+    },
+    _toString: function _toString() {
+        return this._join();
+    },
+    _push: function _push() {
+        for (var i = 0; i < arguments.length; i++) {
+            this[this.length + i] = arguments[i];
+        }
+        this.length += arguments.length;
+        return this.length;
+    },
+    _pop: function _pop() {
+        delete this[this.length -1];
+        this.length = this.length -1;
+        return this.length;
+        // var popLen = this.length -1;
+        // delete this[popLen];
+        // this.length = popLen;
+        // return this.length;
     }
 }
 
@@ -41,7 +65,15 @@ var a = new MyArray('he', 'hello', 'hey');
 console.log(a);
 console.log(a.length);
 console.log(typeof a);
-
+console.log(a._join());
+console.log(a._join(' test '));
+console.log(a._toString());
+console.log(a._push('abc', 'nnn', 'lol'));
+console.log(a._toString());
+console.log(a._pop());
+console.log(a._toString());
+console.log(a._pop());
+console.log(a._toString());
 
 
 // Next Appendix E: Answers to Exercise Questions
