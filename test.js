f34b85b37557e9033cfae8f4a6fc9ef7a48ec608
Marcin Dolecki <mdisoft126@gmail.com>
2023-09-16 12:26:05 +0200

    Appendix E: Answers to Exercise Questions - Chapter 4, Objects ex7 call()

--- test.js
+++ test.js
@@ -9,71 +9,14 @@
 
 ////////// Appendix E: Answers to Exercise Questions
 ////// Chapter 4, Objects
-//// 6
+//// 7
 
-// MyArray()
+// MyMath()
 
-function MyArray(length) {
-    if (typeof length === 'number' && arguments[1] === undefined) {
-        this.length = length;
-        return this;
-    }
-
-    this.length = arguments.length;
-    for (var i = 0, len = arguments.length; i < len; i++) {
-        this[i] = arguments[i];
-    }
-    return this;
-}
-
-MyArray.prototype = {
-    constructor: MyArray,
-    _join: function _join(glue) {
-        var result = '';
-        if (glue === undefined) {
-            glue = ',';
-        }
-        for (var i = 0; i < this.length -1; i++) {
-            result += this[i] === undefined ? '' : this[i];
-            result += glue;
-        }
-        result += this[i] === undefined ? '' : this[i];
-        return result;
-    },
-    _toString: function _toString() {
-        return this._join();
-    },
-    _push: function _push() {
-        for (var i = 0; i < arguments.length; i++) {
-            this[this.length + i] = arguments[i];
-        }
-        this.length += arguments.length;
-        return this.length;
-    },
-    _pop: function _pop() {
-        delete this[this.length -1];
-        this.length = this.length -1;
-        return this.length;
-        // var popLen = this.length -1;
-        // delete this[popLen];
-        // this.length = popLen;
-        // return this.length;
-    }
-}
-
-var a = new MyArray('he', 'hello', 'hey');
-console.log(a);
-console.log(a.length);
-console.log(typeof a);
-console.log(a._join());
-console.log(a._join(' test '));
-console.log(a._toString());
-console.log(a._push('abc', 'nnn', 'lol'));
-console.log(a._toString());
-console.log(a._pop());
-console.log(a._toString());
-console.log(a._pop());
-console.log(a._toString());
+// explanation how works call()
+var arr = [1,2,3];
+console.log(Object.prototype.toString());
+console.log(Object.prototype.toString.call(arr));
 
 
 // Next Appendix E: Answers to Exercise Questions
