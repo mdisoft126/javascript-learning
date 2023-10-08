5ed9054a4cd57ed4afad4bae353d7cd216fc48b1
Marcin Dolecki <mdisoft126@gmail.com>
2023-10-08 13:57:35 +0200

    Appendix E: Answers to Exercise Questions - Chapter 6, Inheritance -  ex2 part2

--- test.js
+++ test.js
@@ -36,20 +36,20 @@ Shape.prototype = {
     constructor: Shape,
 
     // initialization, sets this.context to point to the context if the canvas object
-    init: function() {
-        if(this.context === undefined) {
+    init: function () {
+        if (this.context === undefined) {
             var canvas = document.getElementById("canvas");
             Shape.prototype.context = canvas.getContext("2d");
         }
     },
 
     // method that draws a shape by looping throught this.points
-    draw: function() {
+    draw: function () {
         var i, ctx = this.context;
         ctx.strokeStyle = this.getColor();
         ctx.beginPath();
         ctx.moveTo(this.points[0].x, this.points[0].y);
-        for(i = 1; i < this.points.length; i++) {
+        for (i = 1; i < this.points.length; i++) {
             ctx.lineTo(this.points[i].x, this.points[i].y);
         }
         ctx.closePath();
@@ -57,7 +57,7 @@ Shape.prototype = {
     },
 
     // method that generate a random color
-    getColor: function() {
+    getColor: function () {
         var i, rgb = [];
         for (i = 0; i < 3; i++) {
             rgb[i] = Math.round(255 * Math.random());
@@ -67,23 +67,23 @@ Shape.prototype = {
 
     // method that loops throught the points array,
     // creates Line instances and adds them to this.lines
-    getLines: function() {
+    getLines: function () {
         if (this.lines.length > 0) {
             return this.lines;
         }
         var i, lines = [];
         for (i = 0; i < this.points.length; i++) {
-            lines[i] = new Line(this.points[i], this.points[i +1] || this.points[0]);
+            lines[i] = new Line(this.points[i], this.points[i + 1] || this.points[0]);
         }
         this.lines = lines;
         return lines;
     },
 
     // shell method to be implemented by children
-    getArea: function() {},
+    getArea: function () { },
 
     // sums the lengths of all lines
-    getPerimeter: function() {
+    getPerimeter: function () {
         var i, perim = 0, lines = this.getLines();
         for (i = 0; i < lines.length; i++) {
             perim += lines[i].length;
@@ -96,24 +96,109 @@ function Triangle(a, b, c) {
     this.points = [a, b, c];
     this.getArea = function () {
         var p = this.getPerimeter(),
-        s = p/2;
+            s = p / 2;
         return Math.sqrt(s * (s - this.lines[0].length) *
-        (s - this.lines[1].length) * (s - this.lines[2].length))
+            (s - this.lines[1].length) * (s - this.lines[2].length))
     };
 }
 
-(function () {
-    var s = new Shape();
-    Triangle.prototype = s;
-})();
+function Rectangle(p, side_a, side_b) {
+    this.points = [
+        p,
+        new Point(p.x + side_a, p.y),
+        new Point(p.x + side_a, p.y + side_b),
+        new Point(p.x, p.y + side_b)
+    ];
+    this.getArea = function () {
+        return side_a * side_b;
+    }
+}
 
+function Square(p, side) {
+    Rectangle.call(this, p, side, side);
+}
+
+
+// (function () {
+//     var s = new Shape();
+//     Triangle.prototype = s;
+//     Rectangle.prototype = s;
+//     Square.prototype = s;
+// })();
+
+// examples from the chapter
+/*
 var p1 = new Point(100, 100);
 var p2 = new Point(300, 100);
 var p3 = new Point(200, 0);
 
 var t = new Triangle(p1, p2, p3);
-
 t.draw();
+
+var r = new Rectangle(new Point(200, 200), 50, 100);
+r.draw();
+
+var s = new Square(new Point(130, 130), 50);
+s.draw();
+
+new Square(p1, 200).draw();
+*/
+
+// examples from the exercise 2
+function Kite(center, diag_a, diag_b, height) {
+    this.points = [
+        new Point(center.x - diag_a / 2, center.y),
+        new Point(center.x, center.y + (diag_b - height)),
+        new Point(center.x + diag_a / 2, center.y),
+        new Point(center.x, center.y - height)
+    ];
+    this.getArea = function () {
+        return diag_a * diag_b / 2;
+    };
+}
+function Rhombus(center, diag_a, diag_b) {
+    Kite.call(this, center, diag_a, diag_b, diag_b / 2);
+}
+function Trapezoid(p1, side_a, p2, side_b) {
+    this.points = [p1, p2, new Point(p2.x + side_b, p2.y),
+        new Point(p1.x + side_a, p1.y)
+    ];
+    this.getArea = function () {
+        var height = p2.y - p1.y;
+        return height * (side_a + side_b) / 2;
+    };
+}
+// regular pentagon, all edges have the same length
+function Pentagon(center, edge) {
+
+// finish pentagon!!!!!!!
+
+}
+
+
+function Circle(center, radius) {
+    this.getArea = function () {
+        return Math.pow(radius, 2) * Math.PI;
+    };
+    this.getPerimeter = function () {
+        return 2 * radius * Math.PI;
+    };
+    this.draw = function () {
+        var ctx = this.context;
+        ctx.beginPath();
+        ctx.arc(center.x, center.y, radius, 0, 2 * Math.PI);
+        ctx.stroke();
+    };
+}
+(function () {
+    var s = new Shape();
+    Kite.prototype = s;
+    Rhombus.prototype = s;
+    Trapezoid.prototype = s;
+    Pentagon.prototype = s;
+    Circle.prototype = s;
+}());
+
 
 // after this exercise do an example from gtp
 
